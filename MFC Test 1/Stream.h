4444817f9f1eb2b592d0322b4e88ce574c48cfdd
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-01-07 15:06:46 +0000

    commenting to FileSystem classes

--- MFC Test 1/Stream.h
+++ MFC Test 1/Stream.h
@@ -6,11 +6,19 @@ namespace Library
 {
    namespace IO
    {
+      /// <summary>Defines how a file is opened or created</summary>
       enum class FileMode { CreateNew = CREATE_NEW, CreateAlways = CREATE_ALWAYS, OpenAlways = OPEN_ALWAYS, OpenExisting = OPEN_EXISTING, TruncateExisting = TRUNCATE_EXISTING };
+
+      /// <summary>Defines read/write access on a stream</summary>
       enum class FileAccess : UINT { Read = GENERIC_READ, Write = GENERIC_WRITE, ReadWrite = GENERIC_READ|GENERIC_WRITE };
+
+      /// <summary>Defines how a stream is shared</summary>
       enum class FileShare : UINT { AllowRead = FILE_SHARE_READ, AllowWrite = FILE_SHARE_WRITE, AllowDelete = FILE_SHARE_DELETE, AllowNone = 0 };
+
+      /// <summary>Defines the origin of a stream seeking operation</summary>
       enum class SeekOrigin { Begin = FILE_BEGIN, Current = FILE_CURRENT, End = FILE_END };
 
+      /// <summary>Represents any stream</summary>
       class Stream
       {
       protected:
@@ -37,6 +45,8 @@ namespace Library
          virtual DWORD Read(BYTE* buffer, DWORD length) PURE;
          virtual DWORD Write(const BYTE* buffer, DWORD length) PURE;
       
+         /// <summary>Reads all remaining bytes from a stream into a buffer</summary>
+         /// <returns></returns>
          virtual BYTE* ReadAllBytes()
          {
             DWORD count = 0,
@@ -57,8 +67,10 @@ namespace Library
          }
       };
 
+      /// <summary>A shared pointer to any stream</summary>
       typedef shared_ptr<Stream>  StreamPtr;
 
+      /// <summary>Allows for subclassing a stream using the Facade pattern</summary>
       class StreamFacade : public Stream
       {
       protected:
@@ -76,12 +88,17 @@ namespace Library
          NO_MOVE(StreamFacade);
          NO_COPY(StreamFacade);
 
+         // Accessors
+
          bool  CanRead() const                         { return Source->CanRead();  }
          bool  CanSeek() const                         { return Source->CanSeek();  }
          bool  CanWrite() const                        { return Source->CanWrite(); }
 
          DWORD GetLength()                             { return Source->GetLength(); }
          DWORD GetPosition() const                     { return Source->GetPosition(); }
+
+
+         // Mutators
 
          void  Close()                                 { return Source->Close(); }
          void  Flush()                                 { return Source->Flush(); }
@@ -92,6 +109,8 @@ namespace Library
          DWORD Write(const BYTE* buffer, DWORD length) { return Source->Write(buffer, length); }
 
       private:
+         // Representation
+
          StreamPtr  Source;
       };
       
--- MFC Test 1/XFileInfo.cpp
+++ MFC Test 1/XFileInfo.cpp
@@ -6,12 +6,21 @@ namespace Library
    namespace FileSystem
    {
 
+      // -------------------------------- CONSTRUCTION --------------------------------
+
+      /// <summary>Creates a file descriptor for a physical file</summary>
+      /// <param name="p">Full path of file</param>
       XFileInfo::XFileInfo(Path p) 
          : Source(FileSource::Physical), FullPath(p), FileSystem(nullptr), Catalog(nullptr), Offset(0) 
       {
       }
 
-      XFileInfo::XFileInfo(XFileSystem& vfs, XCatalog& cat, Path subPath, DWORD pos) 
+      /// <summary>Creates a file descriptor for a catalog based file</summary>
+      /// <param name="vfs">The file system</param>
+      /// <param name="cat">The catalog containing the file</param>
+      /// <param name="subPath">The sub path of the file</param>
+      /// <param name="pos">The position within the data-file, in bytes</param>
+      XFileInfo::XFileInfo(XFileSystem& vfs, XCatalog& cat, Path subPath, DWORD pos)
          : Source(FileSource::Catalog), FullPath(vfs.GetFolder()+subPath), FileSystem(&vfs), Catalog(&cat), Offset(pos) 
       {
       }
@@ -19,5 +28,15 @@ namespace Library
       XFileInfo::~XFileInfo() 
       {
       }
+
+      // ------------------------------- PUBLIC METHODS -------------------------------
+
+		// ------------------------------ PROTECTED METHODS -----------------------------
+
+		// ------------------------------- PRIVATE METHODS ------------------------------
+
+		// -------------------------------- NESTED CLASSES ------------------------------
+
+
    }
 }
