799772a5f857e473892aaacb964769a82005d95c
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-01-10 20:09:31 +0000

    added skeleton parameter translation

--- trunk/MFC Test 1/Logic/ScriptParameter.cpp
+++ trunk/MFC Test 1/Logic/ScriptParameter.cpp
@@ -33,10 +33,50 @@ namespace Library
 
       void   ScriptParameter::Translate(ScriptFile& f)
       {
-         if (Value.Type == ValueType::String)
-            Text = Value.String;
-         else
-            Text = GuiString(L"%d", Value.Int);
+
+         switch (Type)
+         {
+         case DataType::DT_VARIABLE:
+            Text = Value.Type == ValueType::String ? Value.String 
+                 : Value.Int >= 0 ? f.Variables[Value.Int].Name
+                 : L"if ";
+            Text.insert(Text.begin(), L'$');
+            break;
+
+         case DataType::DT_STRING:
+            Text = (Syntax.Type == ParameterType::PS_COMMENT ? Value.String : StringResource::Format(L"'%s'", Value.String.c_str()));
+            break;
+
+         case DataType::DT_INTEGER:
+            Text = StringResource::Format(L"%d", Value.Int);
+            break;
+
+         case DataType::DT_NULL:
+            Text = L"null";
+            break;
+
+         case DataType::DT_SECTOR:
+            Text = StringResource::Format(L"[%d]", Value.Int);
+            break;
+
+         case DataType::DT_SCRIPTDEF:
+         case DataType::DT_STATIONSERIAL:
+         case DataType::DT_TRANSPORTCLASS:
+         case DataType::DT_OPERATOR:
+         case DataType::DT_DATATYPE:
+         case DataType::DT_FLIGHTRETURN:
+         case DataType::DT_OBJECTCLASS:
+         case DataType::DT_OBJECTCOMMAND:
+         case DataType::DT_WINGCOMMAND:
+         case DataType::DT_RACE:
+         case DataType::DT_CONSTANT:
+            Text = StringResource::Format(L"[%d]", Value.Int);
+            break;
+
+         case DataType::DT_WARE:
+            Text = StringResource::Format(L"{%d:%d}", HIWORD(Value.Int), LOWORD(Value.Int));
+            break;
+         }
       }
 
 		// ------------------------------ PROTECTED METHODS -----------------------------
