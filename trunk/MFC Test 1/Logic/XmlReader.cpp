933a87d0eb8a2e95c84e004b34f0b1a8a84c2cda
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-01-11 20:33:18 +0000

    LanguageFileReader now skips XML markup/comments

--- trunk/MFC Test 1/Logic/XmlReader.cpp
+++ trunk/MFC Test 1/Logic/XmlReader.cpp
@@ -24,7 +24,7 @@ namespace Logic
          try
          {
             // Create document / Disable async access
-            Document = IXMLDOMDocument2Ptr(CLSID_DOMDocument60);
+            Document = IXMLDOMDocument2Ptr(__uuidof(XML::DOMDocument60));
             Document->async = VARIANT_FALSE;
          }
          catch (_com_error& ex) {
