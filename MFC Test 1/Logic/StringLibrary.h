991a3a318f571af383a0d6f8d3db90d69cf468eb
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-01-11 22:08:32 +0000

    Added special cases to StringLibrary

--- MFC Test 1/Logic/StringLibrary.h
+++ MFC Test 1/Logic/StringLibrary.h
@@ -8,7 +8,28 @@ namespace Logic
 {
    namespace Language
    {
-         
+      /// <summary>Defines the game pages containing the definitions for the various game constants</summary>
+      enum KnownPage : UINT
+      {  
+         SECTORS              = 7,
+         STATION_SERIALS      = 12,
+         SHIPS_STATIONS_WARES = 17,
+         RELATIONS            = 35,
+         RACES                = 1266,
+         TRANSPORT_CLASSES    = 1999,
+         PARAMETER_TYPES      = 2000,
+         OPERATORS            = 2001,
+         CONSTANTS            = 2002,
+         COMMAND_SYNTAX       = 2003,
+         OBJECT_CLASSES       = 2006,
+         CONDITIONALS         = 2007,
+         OBJECT_COMMANDS      = 2008,
+         FLIGHT_RETURNS       = 2009,
+         DATA_TYPES           = 2013,
+         WING_COMMANDS        = 2028 
+      };
+
+
       /// <summary></summary>
       class StringLibrary
       {
@@ -48,12 +69,14 @@ namespace Logic
          void  Clear();
          UINT  Enumerate(XFileSystem& vfs, GameLanguage lang);
 
+      private:
+         LanguageFile  GetSpecialCases(GameLanguage lang);
+
 		   // -------------------- REPRESENTATION ---------------------
 
       public:
          static StringLibrary  Instance;
 
-      private:
          FileCollection  Files;
       };
 
