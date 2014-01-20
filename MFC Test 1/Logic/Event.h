2f6cf1685cef97e5482e592d9f7d999bc0de8eae
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-01-20 02:48:48 +0000

    commented new implementation of event class

--- MFC Test 1/Logic/Event.h
+++ MFC Test 1/Logic/Event.h
@@ -6,63 +6,104 @@
 namespace Logic
 {
 
-
-   //void simpleEventTest();
+   /// <summary>Forward declaration</summary>
+   template<typename ...Args>
+   class Event;
    
-   
+   /// <summary>Abstract base for delegates, only constrained by the type of the method arguments and not its containing class</summary>
+   /// <typeparam name="Args">Member function argument types</typeparam>
    template<typename ...Args>
    class VargDelegate
    {
-   public:
-      typedef VargDelegate<Args...>  Base;
+      friend class Event<Args...>;
 
-      VargDelegate() : Disconnected(false)
+      // ------------------------ TYPES --------------------------
+   
+      // --------------------- CONSTRUCTION ----------------------
+
+   public:
+      VargDelegate() : Connected(true)
       {}
       virtual ~VargDelegate()
       {}
 
-      virtual void Invoke(Args...) PURE;
+      // ------------------------ STATIC -------------------------
 
+      // --------------------- PROPERTIES ------------------------
+			
+      // ---------------------- ACCESSORS ------------------------	
+   public:
+      virtual void Invoke(Args...) const PURE;
+
+      // ----------------------- MUTATORS ------------------------
+   private:
+      /// <summary>Called by an event to indicate it has fallen out of scope, and all delegates are now invalid</summary>
       void OnDisconnect()
       {
-         Disconnected = true;
+         Connected = false;
       }
 
+      // -------------------- REPRESENTATION ---------------------
+
    protected:
-      bool  Disconnected;
+      bool  Connected;
    };
 
 
-   template<typename ...Args>
-   class Event;
-
+   /// <summary>Delegate for invoking class instance methods</summary>
+   /// <typeparam name="Type">Type of class containing the member function to invoke</typeparam>
+   /// <typeparam name="Args">Member function argument types</typeparam>
    template<typename Type, typename ...Args>
    class MethodDelegate : public VargDelegate<Args...>
    {
+      // ------------------------ TYPES --------------------------
    private:
+      /// <summary>Class method function pointer</summary>
       typedef void(Type::*Member)(Args...);
+
+      /// <summary>Type of event to handle</summary>
       typedef Event<Args...>  EventType;
 
-   public:
-      MethodDelegate(EventType& ev, Type* instance, Member method) 
-         : Event(ev), Object(instance), Method(method)
+      /// <summary>Only the event can create this class</summary>
+      friend class EventType;
+
+      // --------------------- CONSTRUCTION ----------------------
+   private:
+      /// <summary>Creates a delegate for an event handler</summary>
+      /// <param name="ev">The event that invokes the delegate</param>
+      /// <param name="instance">The instance that will be invoked</param>
+      /// <param name="method">Pointer to the member function to invoke</param>
+      MethodDelegate(EventType& ev, Type* instance, Member method) : Event(ev), Object(instance), Method(method)
       {
          REQUIRED(instance);
          REQUIRED(method);
       }
+
+   public:
+      /// <summary>Detach from the event upon destruction, unless event has already fallen from scope</summary>
       ~MethodDelegate()
       {
-         if (!Disconnected)
+         if (Connected)
             Event.UnRegister(this);
       }
 
-      void Invoke(Args... args)
+      // ------------------------ STATIC -------------------------
+
+      // --------------------- PROPERTIES ------------------------
+			
+      // ---------------------- ACCESSORS ------------------------			
+
+      // ----------------------- MUTATORS ------------------------
+   public:
+      /// <summary>Invoke the delegate</summary>
+      /// <param name="args">Arguments</param>
+      void Invoke(Args... args) const
       {
-         if (!Disconnected)
+         if (Connected)
             (Object->*Method)(args...);
       }
 
-
+      // -------------------- REPRESENTATION ---------------------
    private:
       Type*       Object;
       Member      Method;
@@ -70,34 +111,52 @@ namespace Logic
    };
 
 
-
+   /// <summary>An event able to invoke class member functions with any signature. 
+   /// Automatically handles disconnection if either party falls out of scope</summary>
+   /// <typeparam name="Args">Event handler argument types</typeparam>
    template<typename ...Args>
    class Event
    {
+      // ------------------------ TYPES --------------------------
    public:
+      /// <summary>Event delegate</summary>
       typedef VargDelegate<Args...> Delegate;
 
    private:
+      /// <summary>Delegate smart pointer</summary>
       typedef shared_ptr<Delegate>  DelegatePtr;
       
+      // --------------------- CONSTRUCTION ----------------------
    public:
-      Event() {}
-      ~Event() 
+      /// <summary>Disconnects any remaining listeners</summary>
+      ~Event()
       {
          for (auto& obj : Listeners)
             obj->OnDisconnect();
       }
 
+      // ------------------------ STATIC -------------------------
+
+      // --------------------- PROPERTIES ------------------------
+			
+      // ---------------------- ACCESSORS ------------------------			
    public:
-      
-      void Raise(Args... args)
+      /// <summary>Raises the event</summary>
+      /// <param name="args">Arguments</param>
+      void Raise(Args... args) const
       {
          for (auto& obj : Listeners)
             obj->Invoke(args...);
       }
 
+      // ----------------------- MUTATORS ------------------------
    public:
-
+      /// <summary>Registers a class method to handle this event</summary>
+      /// <typeparam name="Type">Type of class that will handle the event</typeparam>
+      /// <typeparam name="Member">Type of member function that will handle the event</typeparam>
+      /// <param name="instance">The instance that will handle the event</param>
+      /// <param name="method">Pointer to the member function that will handle event</param>
+      /// <returns>Delegate smart pointer</returns>
       template <typename Type, typename Member>
       DelegatePtr Register(Type* instance, Member method)
       {
@@ -112,13 +171,18 @@ namespace Logic
          return DelegatePtr(p);
       }
 
+      /// <summary>Unregisters a delegate</summary>
+      /// <param name="p">The delegate</param>
       void UnRegister(Delegate* p)
       {
          REQUIRED(p);
 
+         // Remove/detatch delegate
          Listeners.remove(p);
+         p->OnDisconnect();
       }
 
+      // -------------------- REPRESENTATION ---------------------
    private:
       list<Delegate*> Listeners;
    };
