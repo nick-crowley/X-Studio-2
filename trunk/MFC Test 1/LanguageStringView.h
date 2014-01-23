4bb413aeae237e826ff38b2dd58e9b73783b5fc7
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-01-23 16:59:28 +0000

    language page view now populates from document

--- trunk/MFC Test 1/LanguageStringView.h
+++ trunk/MFC Test 1/LanguageStringView.h
@@ -1,7 +1,7 @@
-
 #pragma once
 
 #include "afxcview.h"
+#include "LanguageDocument.h"
 
 /// <summary>User interface</summary>
 NAMESPACE_BEGIN2(GUI,Views)
@@ -32,16 +32,22 @@ NAMESPACE_BEGIN2(GUI,Views)
 	   virtual void AssertValid() const;
 	   virtual void Dump(CDumpContext& dc) const;
    #endif  
+      LanguageDocument* GetDocument() const;
+
       // ----------------------- MUTATORS ------------------------
    protected:
       void AdjustLayout();
 	  
-	  afx_msg void OnSize(UINT nType, int cx, int cy);
+      virtual void OnInitialUpdate();
+	   afx_msg void OnSize(UINT nType, int cx, int cy);
 	  
       // -------------------- REPRESENTATION ---------------------
-      
    private:
    };
    
+   #ifndef _DEBUG  
+   inline LanguageDocument* LanguageStringView::GetDocument() const
+      { return reinterpret_cast<LanguageDocument*>(m_pDocument); }
+   #endif
 
 NAMESPACE_END2(GUI,Views)
