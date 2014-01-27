ef7c629babbace3507f0d7f369e41bf2fd71a608
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-01-27 14:45:02 +0000

    added file iterator to StringLibrary

--- trunk/MFC Test 1/Logic/StringLibrary.h
+++ trunk/MFC Test 1/Logic/StringLibrary.h
@@ -13,88 +13,10 @@ namespace Logic
       /// <summary></summary>
       class StringLibrary
       {
-      private:
-         /// <summary>Sorts language files by descending FileID</summary>
-         class SortByFileID : binary_function<LanguageFile, LanguageFile, bool>
-         {
-         public:
-            bool operator()(const LanguageFile& a, const LanguageFile& b) const
-            { return a.ID > b.ID; }
-         };
-
-         /// <summary>Collection of language files</summary>
-         class FileCollection : public set<LanguageFile, SortByFileID>
-         {
-         };
-
       public:
-         class iterator : public std::iterator<std::forward_iterator_tag, LanguageString>
+         /// <summary>Collection of language files</summary>
+         class FileCollection : public set<LanguageFile, greater<LanguageFile>>
          {
-         public:
-            typedef FileCollection::iterator   FileIterator;
-
-         private:
-            typedef LanguagePage::StringCollection::iterator  StringIterator;
-            typedef LanguageFile::PageCollection::iterator    PageIterator;
-
-            FileCollection* coll;
-            FileIterator    file;
-            PageIterator    page;
-            StringIterator  str;
-
-            /*
-            MyIterator(int* x) :p(x) {}
-           MyIterator(const MyIterator& mit) : p(mit.p) {}
-           MyIterator& operator++() {++p;return *this;}
-           MyIterator operator++(int) {MyIterator tmp(*this); operator++(); return tmp;}
-           bool operator==(const MyIterator& rhs) {return p==rhs.p;}
-           bool operator!=(const MyIterator& rhs) {return p!=rhs.p;}
-           int& operator*() {return *p;}
-            */
-         public:
-            iterator(FileCollection& f, FileIterator pos) : coll(&f), file(pos)
-            {
-               if (file != f.end())
-               {
-                  page = const_cast<LanguageFile::PageCollection&>(file->Pages).begin();  
-                  
-                  if (page != file->Pages.end())
-                     str = (page->second.Strings).begin();  
-               }
-            }
-
-            iterator& operator++() 
-            { 
-               // Advance string
-               if (str != page->second.Strings.end())
-               {
-                  //if (++str == page->second.Strings.end())
-                  ++str;
-               }
-               // Advance page
-               else if (page != file->Pages.end())
-                  ++page;
-               // Advance file
-               else if (file != coll->end())
-                  ++file;
-
-               return *this;
-            }
-
-            iterator operator++(int) 
-            {
-               iterator tmp(*this); 
-               operator++(); 
-               return tmp;
-            }
-
-            LanguageString& operator*() 
-            {
-               return str->second;
-            }
-            
-            bool operator==(const iterator& r) const { return coll==r.coll && file==r.file && page==r.page && str==r.str; }
-            bool operator!=(const iterator& r) const { return coll!=r.coll || file!=r.file || page!=r.page || str!=r.str; }
          };
 
          // --------------------- CONSTRUCTION ----------------------
@@ -107,11 +29,13 @@ namespace Logic
          // ------------------------ STATIC -------------------------
 
          // --------------------- PROPERTIES ------------------------
-         iterator begin() { return iterator(Files, Files.begin()); }
-         iterator end();
+         
 		   // ---------------------- ACCESSORS ------------------------			
 
       public:
+         FileCollection::const_iterator begin() const { return Files.begin(); }
+         FileCollection::const_iterator end() const   { return Files.end(); }
+
          bool            Contains(UINT page, UINT id) const;
          LanguageString  Find(UINT page, UINT id) const;
          wstring         Resolve(UINT page, UINT id) const;
