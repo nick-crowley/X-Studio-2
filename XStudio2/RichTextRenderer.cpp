3bb75a3f98d1d3b052c44c537cd2b0305effa4b1
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-02-13 17:31:21 +0000

    Added initial implementation of RichText single line renderer

--- XStudio2/RichTextRenderer.cpp
+++ XStudio2/RichTextRenderer.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "RichTextRenderer.h"
+#include "Logic/RtfStringWriter.h"
 
 namespace GUI
 {
@@ -19,13 +20,96 @@ namespace GUI
 
       // ------------------------------- STATIC METHODS -------------------------------
 
+      struct RichPhrase
+      {
+      public:
+         RichPhrase(RichCharacter ch) : Format(ch.Format), Colour(ch.Colour)
+         {
+            Text.push_back(ch.Char);
+         }
+
+         wstring Text;
+         UINT    Format;
+         Colour  Colour;
+      };
+
       /// <summary>Draw rich text in a single line</summary>
       /// <param name="dc">dc.</param>
       /// <param name="line">line rect</param>
       /// <param name="str">string.</param>
       void  RichTextRenderer::DrawLine(CDC* dc, CRect line, const RichString& str)
       {
-         dc->DrawText(L"quick brown fox", line, DT_LEFT);
+         RichCharList chars;
+
+         // Flatten string into list of characters
+         for (auto& para : str.Paragraphs)
+            for (auto ch : para.Characters)
+               if (ch->Char != '\n')
+                  chars.push_back(ch);
+
+         // Abort if empty string
+         if (chars.empty())
+            return;
+
+         list<RichPhrase> phrases;
+         phrases.push_back(RichPhrase(*chars.front()));
+
+         // Transform into blocks of contiguous characters
+         for_each(++chars.begin(), chars.end(), [&](const RichCharacter* ch) 
+         {
+            // Create new phrase if colour/formatting changes
+            if (ch->Colour != phrases.back().Colour
+             || ch->Format != phrases.back().Format)
+                phrases.push_back(RichPhrase(*ch));
+            else
+               // Otherwise append to last phrase
+               phrases.back().Text.push_back(ch->Char);
+         });
+
+         LOGFONT fontData;
+         CFont* oldFont;
+
+         // Get original font properties
+         oldFont = dc->GetCurrentFont();
+         if (!oldFont->GetLogFont(&fontData))
+            throw Win32Exception(HERE, L"Unable to get logical font");
+         
+         // Draw phrases
+         for (auto& block : phrases)
+         {
+            CFont f;
+            LOGFONT lf = fontData;
+            Colour col = block.Colour;
+
+            // Create/Select approprate font
+            fontData.lfItalic = (block.Format & CFE_ITALIC ? TRUE : FALSE);
+            fontData.lfWeight = (block.Format & CFE_BOLD ? FW_BOLD : FW_NORMAL);
+            fontData.lfUnderline = (block.Format & CFE_UNDERLINE ? TRUE : FALSE);
+            f.CreateFontIndirectW(&lf);
+            dc->SelectObject(&f);
+            
+            // Invert black/white
+            switch (block.Colour)
+            {
+            case Colour::Default:
+            case Colour::White:   
+            case Colour::Silver:  col = Colour::Black;  break;
+            case Colour::Black:   col = Colour::White;  break;
+            }
+            // Set colour
+            dc->SetTextColor(RtfStringWriter::ToRGB(col));
+
+            // Draw as much text as possible
+            dc->DrawText(block.Text.c_str(), block.Text.length(), line, DT_LEFT|DT_END_ELLIPSIS|DT_SINGLELINE|DT_VCENTER);
+            line.left += dc->GetTextExtent(block.Text.c_str(), block.Text.length()).cx;
+
+            // Restore
+            dc->SelectObject(oldFont);
+
+            // EOF: Abort if we run out of space
+            if (line.left > line.right)
+               break;
+         }
       }
 
       // ------------------------------- PUBLIC METHODS -------------------------------
