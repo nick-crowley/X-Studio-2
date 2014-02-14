22fa85931fccd0c590ba052070340a1f69f4172a
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-02-14 17:15:33 +0000

    formatting + commenting

--- trunk/XStudio2/Logic/DescriptionFileReader.cpp
+++ trunk/XStudio2/Logic/DescriptionFileReader.cpp
@@ -4,11 +4,23 @@
 #include <strsafe.h>
 #include "StringResolver.h"
 
+/// <summary>Turn console debugging output on/off</summary>
+#define PRINT_CONSOLE
+
 namespace Logic
 {
    namespace IO
    {
    
+      /// <summary>Matches a capitalized keyword or a macro with no parameters, in both instances the name is captured</summary>
+      const wregex  DescriptionFileReader::MatchKeyword(L"\\b([A-Z_0-9]+)(?![:\\}])\\b" L"|" L"\\{([A-Z_0-9]+)\\}");
+
+      /// <summary>Matches a macro with one or more parameters. captures the name alone and parameters as a block</summary>
+      const wregex  DescriptionFileReader::MatchMacro(L"\\{([A-Z_0-9]+):([^\\}]+)\\}");
+
+      /// <summary>Matches each parameter in a comma delimited string</summary>
+      const wregex  DescriptionFileReader::MatchParameters(L"([^,]+)((?=,)[^,]+)*");
+
       // -------------------------------- CONSTRUCTION --------------------------------
 
       /// <summary>Creates a description xml file reader from an input stream</summary>
@@ -71,7 +83,7 @@ namespace Logic
                {
                   ConstantDescription& d = c.second;
                   
-                  //if (d.Page == 2006 && d.ID == 2020)
+                  if (d.Page == 2006 && d.ID == 2020)
                   {
                      Console << "Parsing constant: page=" << d.Page << " id=" << d.ID << " txt='" << Colour::Cyan << d.Text << "'" << ENDL;
                      Console << ENDL << ENDL;
@@ -99,6 +111,134 @@ namespace Logic
 
       // ------------------------------- PRIVATE METHODS ------------------------------
    
+      /// <summary>Called for each occurrence of parameterized macros</summary>
+      /// <param name="match">The match.</param>
+      /// <param name="depth">Debugging output depth</param>
+      /// <returns>Replacement text</returns>
+      wstring  DescriptionFileReader::onMatchMacro(const wsmatch& match, int depth) const 
+      {
+         const Macro* macro;
+         wstring name      = match[1].str(),
+                 arguments = match[2].str();
+
+         // Lookup macro 
+         if (Macros.TryFind(name, macro))
+         {
+            vector<wstring> arg = { L"", L"", L"", L"", L"", L"" };
+            UINT i = 0;
+
+            // Separate parameters into array
+            for (wsregex_iterator m(arguments.begin(), arguments.end(), MatchParameters), end; m != end && i <= 5; ++m)
+               arg[i++] = (*m)[0].str();
+
+            // Verify argument count
+            if (macro->ParamCount != i)
+               throw FileFormatException(HERE, GuiString(L"The macro '%s' requires %d parameters : '%s'", macro->Name.c_str(), macro->ParamCount, match[0].str().c_str()));
+
+            // Format macro with up to six parameters...
+            StringCchPrintf(FormatBuffer.get(), BUFFER_LENGTH, 
+                            macro->Text.c_str(), 
+                            arg[0].c_str(), arg[1].c_str(), arg[2].c_str(), arg[3].c_str(), arg[4].c_str(), arg[5].c_str());
+
+#ifdef PRINT_CONSOLE
+            Console << Colour::Cyan << Indent(depth) << "Matched Macro: " << Colour::Yellow << match[0].str() 
+                    << Colour::White << " with " << Colour::Yellow << FormatBuffer.get() << ENDL;
+#endif
+            // Recursively parse
+            return Parse(FormatBuffer.get(), depth+1);
+         }
+
+#ifdef PRINT_CONSOLE
+         Console << Colour::Red << Indent(depth) << "Ignored Macro: " << Colour::White << match[0].str() << ENDL;
+#endif
+
+         // Failed: Return verbatim
+         return match[0].str();
+      }
+
+      /// <summary>Called for each occurrence of parameterless macros</summary>
+      /// <param name="match">The match.</param>
+      /// <param name="depth">Debugging output depth</param>
+      /// <returns>Replacement text</returns>
+      wstring  DescriptionFileReader::onMatchKeyword(const wsmatch& match, int depth) const 
+      {
+         const Macro* m;
+         wstring name = (match[1].matched ? match[1].str() : match[2].str()); // Retrieve keyword/macro
+         
+         // Lookup macro + recursively parse
+         if (Macros.TryFind(name, m))
+         {
+#ifdef PRINT_CONSOLE
+            Console << Colour::Cyan << Indent(depth) << "Matched Keyword: " << Colour::Yellow << match[0].str()
+                    << Colour::White << " with " << Colour::Yellow << m->Text << ENDL;
+#endif
+
+            return m->Recursive ? Parse(m->Text, depth+1) : m->Text;
+         }
+
+#ifdef PRINT_CONSOLE
+         Console << Colour::Red << Indent(depth) << "Ignored Keyword: " << Colour::White << match[0].str() << ENDL;
+#endif
+
+         // Failed: Return match
+         return match[0].str();
+      }
+
+
+      /// <summary>Parses all the keywords/macros in a string and replaces them recursively</summary>
+      /// <param name="text">Source text to parse</param>
+      /// <param name="depth">Debugging output depth</param>
+      /// <returns>Fully parsed text</returns>
+      wstring  DescriptionFileReader::Parse(wstring text, int depth) const 
+      {
+         UINT Position;
+         wsmatch match;
+         wstring r;
+         
+         try
+         {
+#ifdef PRINT_CONSOLE
+            Console << Colour::Cyan << Indent(depth) << "Parsing: " << Colour::White << text << ENDL;
+#endif
+
+            // Find/Replace all macros:  {AAAA:bbb}, {AAAA:bbb,ccc}, {AAAA:bbb,ccc,ddd} ...
+            for (Position = 0; regex_search(text.cbegin()+Position, text.cend(), match, MatchMacro); ) //  Manually track position for in-place replacement + avoid infinite loop
+            {
+               // Recursively generate replacement text
+               r = onMatchMacro(match, depth);
+
+#ifdef PRINT_CONSOLE
+               Console << Indent(depth) << "Replacing text: " << Colour::Yellow << match[0].str() << Colour::White << " with " << Colour::Green << r << ENDL;
+#endif
+
+               // Advance position to beyond inserted text, and insert text
+               Position = (match[0].first - text.cbegin()) + r.length();
+               text.replace(match[0].first, match[0].second, r);
+            }
+
+            // Find/Replace all keywords:  {AAAAA}, AAAAA
+            for (Position = 0; regex_search(text.cbegin()+Position, text.cend(), match, MatchKeyword); )  
+            {
+               // Recursively generate replacement text
+               r = onMatchKeyword(match, depth);
+
+#ifdef PRINT_CONSOLE
+               Console << Indent(depth) << "Replacing text: " << Colour::Yellow << match[0].str() << Colour::White << " with " << Colour::Green << r << ENDL;
+#endif
+
+               // Advance position to beyond inserted text, and insert text
+               Position = (match[0].first - text.cbegin()) + r.length();
+               text.replace(match[0].first, match[0].second, r);
+            }
+
+            return text;
+         }
+         catch (regex_error& e) {
+            throw RegularExpressionException(HERE, e);
+         }
+      }
+
+
       /// <summary>Reads a command description</summary>
       /// <param name="n">command node</param>
       /// <returns></returns>
@@ -131,26 +271,6 @@ namespace Logic
          return CommandDescription(id, ver, (wchar*)n->text);  
       }
 
-      /// <summary>Reads the command descriptions</summary>
-      /// <param name="file">description file</param>
-      void  DescriptionFileReader::ReadCommands(DescriptionFile& file)
-      {
-         // Find macros node
-         auto commandsNode = Root->selectSingleNode(L"commands");
-         if (commandsNode == nullptr)
-            throw FileFormatException(HERE, L"Missing 'commands' node");
-
-         // Read macros
-         for (int i = 0; i < commandsNode->childNodes->length; i++)
-         {
-            XmlNodePtr n = commandsNode->childNodes->item[i];
-
-            // Read all elements
-            if (n->nodeType == XML::NODE_ELEMENT)
-               file.Commands.Add( ReadCommand(n) );
-         }
-      }
-
 
       /// <summary>Reads a script object description</summary>
       /// <param name="n">constant node</param>
@@ -171,26 +291,7 @@ namespace Logic
          return ConstantDescription(page, id, (wchar*)n->text);  
       }
 
-      /// <summary>Reads the script object descriptions</summary>
-      /// <param name="file">description file</param>
-      void  DescriptionFileReader::ReadConstants(DescriptionFile& file)
-      {
-         // Find macros node
-         auto constantsNode = Root->selectSingleNode(L"constants");
-         if (constantsNode == nullptr)
-            throw FileFormatException(HERE, L"Missing 'constants' node");
-
-         // Read macros
-         for (int i = 0; i < constantsNode->childNodes->length; i++)
-         {
-            XmlNodePtr n = constantsNode->childNodes->item[i];
-
-            // Read all elements
-            if (n->nodeType == XML::NODE_ELEMENT)
-               file.Constants.Add( ReadConstant(n) );
-         }
-      }
-
+      
       /// <summary>Reads a macro element</summary>
       /// <param name="n">Macro node</param>
       /// <returns></returns>
@@ -218,6 +319,49 @@ namespace Logic
          return Macro(name, txt, params, recurse);  
       }
 
+      
+      /// <summary>Reads the command descriptions</summary>
+      /// <param name="file">description file</param>
+      void  DescriptionFileReader::ReadCommands(DescriptionFile& file)
+      {
+         // Find macros node
+         auto commandsNode = Root->selectSingleNode(L"commands");
+         if (commandsNode == nullptr)
+            throw FileFormatException(HERE, L"Missing 'commands' node");
+
+         // Read macros
+         for (int i = 0; i < commandsNode->childNodes->length; i++)
+         {
+            XmlNodePtr n = commandsNode->childNodes->item[i];
+
+            // Read all elements
+            if (n->nodeType == XML::NODE_ELEMENT)
+               file.Commands.Add( ReadCommand(n) );
+         }
+      }
+
+
+      /// <summary>Reads the script object descriptions</summary>
+      /// <param name="file">description file</param>
+      void  DescriptionFileReader::ReadConstants(DescriptionFile& file)
+      {
+         // Find macros node
+         auto constantsNode = Root->selectSingleNode(L"constants");
+         if (constantsNode == nullptr)
+            throw FileFormatException(HERE, L"Missing 'constants' node");
+
+         // Read macros
+         for (int i = 0; i < constantsNode->childNodes->length; i++)
+         {
+            XmlNodePtr n = constantsNode->childNodes->item[i];
+
+            // Read all elements
+            if (n->nodeType == XML::NODE_ELEMENT)
+               file.Constants.Add( ReadConstant(n) );
+         }
+      }
+
+
       /// <summary>Reads the macros.</summary>
       void  DescriptionFileReader::ReadMacros()
       {
@@ -234,117 +378,6 @@ namespace Logic
             // Read all elements
             if (n->nodeType == XML::NODE_ELEMENT)
                Macros.Add( ReadMacro(n) );
-         }
-      }
-
-
-      /// <summary>Called for each occurrence of parameterized macros</summary>
-      /// <param name="match">The match.</param>
-      /// <returns>Replacement text</returns>
-      wstring  DescriptionFileReader::onMatchMacro(wsmatch& match)
-      {
-         const Macro* macro;
-         wstring name      = match[1].str(),
-                 arguments = match[2].str();
-
-         // DEBUG:
-         //Console << Colour::Cyan << "Matched Macro: " << Colour::White << match[0].str() << ENDL;
-
-         // Lookup macro 
-         if (Macros.TryFind(name, macro))
-         {
-            vector<wstring> arg = { L"", L"", L"", L"", L"", L"" };
-            UINT i = 0;
-
-            // Separate parameters into array
-            for (wsregex_iterator m(arguments.begin(), arguments.end(), MatchParameters), end; m != end && i <= 5; ++m)
-               arg[i++] = (*m)[0].str();
-
-            // Verify argument count
-            if (macro->ParamCount != i)
-               throw FileFormatException(HERE, GuiString(L"The macro '%s' requires %d parameters : '%s'", macro->Name.c_str(), macro->ParamCount, match[0].str().c_str()));
-
-            // Format macro with up to six parameters...
-            StringCchPrintf(FormatBuffer.get(), BUFFER_LENGTH, 
-                            macro->Text.c_str(), 
-                            arg[0].c_str(), arg[1].c_str(), arg[2].c_str(), arg[3].c_str(), arg[4].c_str(), arg[5].c_str());
-
-            // Recursively parse
-            return Parse(FormatBuffer.get());
-         }
-
-         // Failed: Return verbatim
-         return match[0].str();
-      }
-
-      /// <summary>Called for each occurrence of parameterless macros</summary>
-      /// <param name="match">The match.</param>
-      /// <returns>Replacement text</returns>
-      wstring  DescriptionFileReader::onMatchKeyword(wsmatch& match)
-      {
-         const Macro* m;
-         wstring name = (match[1].matched ? match[1].str() : match[2].str()); // Retrieve keyword/macro
-         
-         // DEBUG:
-         //Console << Colour::Cyan << "Matched Keyword: " << Colour::White << match[0].str() << ENDL;
-
-         // Lookup macro + recursively parse
-         if (Macros.TryFind(name, m))
-            return m->Recursive ? Parse(m->Text) : m->Text;
-
-         // Failed: Return match
-         return match[0].str();
-      }
-
-      /// <summary>Matches a capitalized keyword or a macro with no parameters, in both instances the name is captured</summary>
-      wregex  DescriptionFileReader::MatchKeyword(L"\\b([A-Z_0-9]+)(?![:\\}])\\b" L"|" L"\\{([A-Z_0-9]+)\\}");
-
-      /// <summary>Matches a macro with one or more parameters. captures the name alone and parameters as a block</summary>
-      wregex  DescriptionFileReader::MatchMacro(L"\\{([A-Z_0-9]+):([^\\}]+)\\}");
-
-      /// <summary>Matches each parameter in a comma delimited string</summary>
-      wregex  DescriptionFileReader::MatchParameters(L"([^,]+)((?=,)[^,]+)*");
-
-      // ------------------------------- PRIVATE METHODS ------------------------------
-
-      /// <summary>Parses the text.</summary>
-      wstring  DescriptionFileReader::Parse(wstring text)
-      {
-         UINT Position;
-         wsmatch match;
-         wstring r;
-         
-         try
-         {
-            // DEBUG:
-            //Console << Colour::Cyan << "Parsing: " << Colour::White << text << ENDL;
-
-            // Replace parameterized macros.   
-            for (Position = 0; regex_search(text.cbegin()+Position, text.cend(), match, MatchMacro); ) //  Manually track position for in-place replacement + avoid infinite loop
-            {
-               r = onMatchMacro(match);
-               Console << "  Replace Macro: " << Colour::Yellow << match[0].str() << Colour::White << " with " << Colour::Green << r << ENDL;
-
-               // Advance position to beyond inserted text, and insert text
-               Position = (match[0].first - text.cbegin()) + r.length();
-               text.replace(match[0].first, match[0].second, r);
-            }
-
-            // Replace keywords
-            for (Position = 0; regex_search(text.cbegin()+Position, text.cend(), match, MatchKeyword); )  
-            {
-               r = onMatchKeyword(match);
-               Console << "  Replace Keyword: " << Colour::Yellow << match[0].str() << Colour::White << " with " << Colour::Green << r << ENDL;
-
-               // Advance position to beyond inserted text, and insert text
-               Position = (match[0].first - text.cbegin()) + r.length();
-               text.replace(match[0].first, match[0].second, r);
-            }
-
-            return text;
-         }
-         catch (regex_error& e) {
-            throw RegularExpressionException(HERE, e);
          }
       }
 
