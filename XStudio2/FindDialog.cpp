9a72463ff34b22711d205c670cca16e76c064ea9
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-02-16 13:22:28 +0000

    minor tweaks

--- XStudio2/FindDialog.cpp
+++ XStudio2/FindDialog.cpp
@@ -29,11 +29,14 @@ NAMESPACE_BEGIN2(GUI,Window)
       IMPLEMENT_DYNAMIC(FindDialog, CDialogEx)
       
       BEGIN_MESSAGE_MAP(FindDialog, CDialogEx)
-         ON_BN_CLICKED(IDC_FIND, &FindDialog::OnFind)
-         ON_BN_CLICKED(IDC_REPLACE, &FindDialog::OnReplace)
-         ON_BN_CLICKED(IDC_FIND_ALL, &FindDialog::OnFindAll)
-         ON_BN_CLICKED(IDC_REPLACE_ALL, &FindDialog::OnReplaceAll)
-         ON_BN_CLICKED(IDC_OPTIONS, &FindDialog::OnOptions)
+         ON_BN_CLICKED(IDC_FIND, &FindDialog::OnFind_Click)
+         ON_BN_CLICKED(IDC_REPLACE, &FindDialog::OnReplace_Click)
+         ON_BN_CLICKED(IDC_FIND_ALL, &FindDialog::OnFindAll_Click)
+         ON_BN_CLICKED(IDC_REPLACE_ALL, &FindDialog::OnReplaceAll_Click)
+         ON_BN_CLICKED(IDC_OPTIONS, &FindDialog::OnOptions_Click)
+         ON_CBN_EDITCHANGE(IDC_FIND_COMBO, &FindDialog::OnFind_TextChanged)
+         ON_CBN_EDITCHANGE(IDC_REPLACE_COMBO, &FindDialog::OnReplace_TextChanged)
+         ON_CBN_EDITCHANGE(IDC_TARGET_COMBO, &FindDialog::OnTarget_TextChanged)
       END_MESSAGE_MAP()
 
       // ------------------------------- PUBLIC METHODS -------------------------------
@@ -55,34 +58,62 @@ NAMESPACE_BEGIN2(GUI,Window)
          DDX_Control(pDX, IDC_TARGET_COMBO, TargetCombo);
       }
       
-      void FindDialog::OnFind()
+      BOOL FindDialog::OnInitDialog()
+      {
+         CDialogEx::OnInitDialog();
+
+         // Populate targets
+         TargetCombo.AddString(L"Selection");
+         TargetCombo.AddString(L"Current Document");
+         TargetCombo.AddString(L"All Open Documents");
+         TargetCombo.AddString(L"All Project Files");
+         TargetCombo.AddString(L"Scripts Folder");
+
+         return TRUE;  // return TRUE unless you set the focus to a control
+      }
+
+      void FindDialog::OnFind_Click()
       {
          // TODO: Add your control notification handler code here
       }
 
-
-      void FindDialog::OnReplace()
+      
+      void FindDialog::OnFind_TextChanged()
+      {
+         // TODO: Add your control notification handler code here
+      }
+      
+      void FindDialog::OnFindAll_Click()
+      {
+         // TODO: Add your control notification handler code here
+      }
+      
+      void FindDialog::OnOptions_Click()
       {
          // TODO: Add your control notification handler code here
       }
 
-
-      void FindDialog::OnFindAll()
+      void FindDialog::OnReplace_Click()
       {
          // TODO: Add your control notification handler code here
       }
 
-
-      void FindDialog::OnReplaceAll()
+      void FindDialog::OnReplace_TextChanged()
       {
          // TODO: Add your control notification handler code here
       }
+      
 
-
-      void FindDialog::OnOptions()
+      void FindDialog::OnReplaceAll_Click()
       {
          // TODO: Add your control notification handler code here
       }
+      
+      void FindDialog::OnTarget_TextChanged()
+      {
+         // TODO: Add your control notification handler code here
+      }
+      
 
       // ------------------------------- PRIVATE METHODS ------------------------------
 
@@ -90,4 +121,5 @@ NAMESPACE_BEGIN2(GUI,Window)
 
 /// <summary>User interface windows</summary>
 NAMESPACE_END2(GUI,Window)
+
 
