db60526e2a91bcfd6955731f66386c18d75f88ef
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-02-19 23:46:28 +0000

    aborted experiment into search by text selection

--- trunk/XStudio2/Logic/MatchData.h
+++ trunk/XStudio2/Logic/MatchData.h
@@ -88,27 +88,29 @@ namespace Logic
       {
          int pos = GuiString::npos, len = 0;
 
-         // Validate position in case text has shrunk
+         // Validate position (in case text has been altered since previous call)
          if (start >= input.length())
          {
             Clear();
             return false;
          }
          
-         // RegEx: Find from last match using regEx
+         // RegEx: 
          if (UseRegEx)
          {
             wsmatch matches;
-            if (regex_search(input.cbegin()+start, input.cend(), matches, RegEx, regex_constants::match_default))
+            // Search from 'start' within search range 
+            if (regex_search(input.cbegin()+start, input.cend(), matches, RegEx))  //, regex_constants::match_default))
             {
                pos = matches[0].first - input.cbegin();
                len = matches[0].length();
             }
          }
-         // Basic: Find from last match using wstring
+         // Basic: 
          else
          {
-            pos = input.Find(SearchTerm, start, MatchCase);
+            // Search from 'start' within search range 
+            pos = input.Find(SearchTerm, 0, MatchCase);
             len = SearchTerm.length();
          }
          
@@ -128,6 +130,7 @@ namespace Logic
       /// <summary>Get the replacement text for the current match</summary>
       /// <param name="input">input text</param>
       /// <returns></returns>
+      /// <exception cref="Logic::AlgorithmException">Previous match no longer matches regEx</exception>
       wstring  Replace(const GuiString& input) const
       {
          // Basic: Return replacement text
@@ -149,30 +152,28 @@ namespace Logic
       /// <summary>Sets the match.</summary>
       /// <param name="start">start index.</param>
       /// <param name="length">length.</param>
-      /// <param name="txt">text.</param>
-      /// <param name="txt">1-based line number.</param>
+      /// <param name="line">1-based line number.</param>
       void  SetMatch(int start, int length, int line)
       {
          Location = {start, start+length};
          IsMatched = true;
          LineNumber = line;
-         //Text = txt;
       }
 
       // -------------------- REPRESENTATION ---------------------
    public:
-      bool        IsMatched;
-      CHARRANGE   Location;
-      IO::Path    FullPath;
-      int         LineNumber;
-      wstring     LineText;
+      bool        IsMatched;        // Whether match was successful
+      CHARRANGE   Location;         // Location of match
+      IO::Path    FullPath;         // File: Fullpath   Document: Title
+      int         LineNumber;       // 1-based line number of match
+      wstring     LineText;         // Text of line containing match
 
-      const wstring  SearchTerm,
-                     ReplaceTerm;
-      const bool     MatchCase,
-                     MatchWord,
-                     UseRegEx;
-      const wregex   RegEx;
+      const wstring   SearchTerm,    // Search term/expression
+                      ReplaceTerm;   // Replacement term/expression
+      const bool      MatchCase,     // Match case 
+                      MatchWord,     // Match whole word
+                      UseRegEx;      // Search and replacement terms are regular expressions
+      const wregex    RegEx;         // Search RegEx, if 'UseRegEx'
    };
 
    
