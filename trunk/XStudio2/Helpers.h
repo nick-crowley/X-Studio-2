543249cccb764bb61c34e6d290a03cb0516384ff
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-02-21 20:00:13 +0000

    reworked how items are project treeview items are populated

--- trunk/XStudio2/Helpers.h
+++ trunk/XStudio2/Helpers.h
@@ -1,6 +1,6 @@
-
 #pragma once
 #include "stdafx.h"
+#include <strsafe.h>
 
 /// <summary>User interface</summary>
 NAMESPACE_BEGIN2(GUI,Controls)
@@ -86,15 +86,14 @@ NAMESPACE_BEGIN2(GUI,Controls)
       }
       
       // ------------------------ STATIC -------------------------
-      
+   private:
       /// <summary>Duplicates the input string.</summary>
       /// <param name="str">The string.</param>
       /// <returns>new wide char array</returns>
-      wchar* CopyText(const wstring& str)
+      static wchar* CopyText(const wstring& str)
       {
          wchar* buf = new wchar[str.length()+1];
-         memcpy(buf, str.data(), sizeof(wchar)*str.length());
-         buf[str.length()] = L'\0';
+         StringCchCopy(buf, str.length()+1, str.c_str());
          return buf;
       }
 
@@ -105,8 +104,78 @@ NAMESPACE_BEGIN2(GUI,Controls)
       // ----------------------- MUTATORS ------------------------
       
       // -------------------- REPRESENTATION ---------------------
-      
+   protected:
+      CharArrayPtr  Text;
+   };
+
+
+   /// <summary>TreeView item helper</summary>
+   class TVItem : public TVITEMEX
+   {
+      // ------------------------ TYPES --------------------------
    private:
+	  
+      // --------------------- CONSTRUCTION ----------------------
+      
+   public:
+      /// <summary>Create empty item for retrieving text and properties</summary>
+      /// <param name="node">node</param>
+      /// <param name="bufLen">buffer length</param>
+      /// <param name="flags">desired properties</param>
+      TVItem(UINT bufLen, UINT flags = TVIF_TEXT | TVIF_STATE | TVIF_IMAGE | TVIF_SELECTEDIMAGE | TVIF_PARAM)
+      {
+         ZeroMemory(dynamic_cast<TVITEMEX*>(this), sizeof(TVITEMEX));
+
+         // Allocate text buffer
+         Text = CharArrayPtr(new wchar[bufLen+1]);
+         Text.get()[0] = L'\0';
+
+         // Set properties
+         mask       = flags;
+         pszText    = Text.get();
+         cchTextMax = bufLen;
+      }
+
+      /// <summary>Create a populated item for setting text and properties</summary>
+      /// <param name="node">node</param>
+      /// <param name="txt">text.</param>
+      /// <param name="flags">properties.</param>
+      /*TVItem(const wstring& txt, UINT flags = TVIF_TEXT) : Text(CopyText(txt))
+      {
+         ZeroMemory(dynamic_cast<TVITEMEX*>(this), sizeof(TVITEMEX));
+
+         mask     = flags;
+         pszText  = Text.get();
+      }*/
+      
+      // ------------------------ STATIC -------------------------
+   private:
+      /// <summary>Duplicates the input string.</summary>
+      /// <param name="str">The string.</param>
+      /// <returns>new wide char array</returns>
+      /*static wchar* CopyText(const wstring& str)
+      {
+         wchar* buf = new wchar[str.length()+1];
+         StringCchCopy(buf, str.length()+1, str.c_str());
+         return buf;
+      }*/
+
+      // --------------------- PROPERTIES ------------------------
+	   
+      // ---------------------- ACCESSORS ------------------------			
+   public:
+      
+      // ----------------------- MUTATORS ------------------------
+   public:
+      /// <summary>Sets the text</summary>
+      /// <param name="str">The string.</param>
+      void  SetText(const wstring& str)
+      {
+         StringCchCopy(Text.get(), cchTextMax, str.c_str());
+      }
+
+      // -------------------- REPRESENTATION ---------------------
+   protected:
       CharArrayPtr  Text;
    };
 
--- trunk/XStudio2/ProjectTreeCtrl.h
+++ trunk/XStudio2/ProjectTreeCtrl.h
@@ -1,17 +1,76 @@
 #pragma once
+#include "Helpers.h"
+#include "ProjectDocument.h"
+//#include "Logic/ProjectFile.h"
 
 /// <summary>User interface</summary>
-FORWARD_DECLARATION2(Logic,Projects,class ProjectItem)
+//FORWARD_DECLARATION2(Logic,Projects,class ProjectItem)
 
 /// <summary>User interface controls</summary>
 NAMESPACE_BEGIN2(GUI,Controls)
 
-   /// <summary></summary>
+   /// <summary>Project window treeview control</summary>
    class ProjectTreeCtrl : public CTreeCtrl
    {
       // ------------------------ TYPES --------------------------
    private:
-	  
+      /// <summary>Item in the tree</summary>
+	   class TreeItem : public TVItem
+      {
+      public:
+         /// <summary>Create from project document</summary>
+         /// <param name="item">The item.</param>
+         TreeItem(ProjectDocument* doc) : TVItem(MAX_PATH), Data(nullptr)
+         {
+            // Use document title
+            SetText((LPCWSTR)doc->GetTitle());
+
+            // Set icon
+            iImage = iSelectedImage = 1;
+            
+            // Bold + Expanded
+            state = stateMask = TVIS_BOLD | TVIS_EXPANDED;
+         }
+
+         /// <summary>Create from project item</summary>
+         /// <param name="item">The item.</param>
+         TreeItem(ProjectItem* item) : TVItem(MAX_PATH), Data(item)
+         {
+            // Generate name
+            if (auto var = dynamic_cast<ProjectVariableItem*>(item))
+               SetText(GuiString(L"%s = %d", var->Name.c_str(), var->Value));
+            else
+               SetText(item->Name);
+
+            // Choose icon
+            switch (item->Type)
+            {
+            case ProjectItemType::Folder:    iImage = 0;  break;
+            case ProjectItemType::Variable:  iImage = 6;  break;
+            case ProjectItemType::File:      
+               switch (dynamic_cast<ProjectFileItem*>(item)->FileType)
+               {
+               case FileType::Script:   iImage = 2;  break;
+               case FileType::Language: iImage = 3;  break;
+               case FileType::Mission:  iImage = 4;  break;
+               case FileType::Unknown:  iImage = 5;  break;
+               }
+               break;
+            }
+            iSelectedImage = iImage;
+
+            // Fixed: Bold + Expanded
+            if (item->Fixed)
+               state = stateMask = TVIS_BOLD | TVIS_EXPANDED;
+
+            // Item data:
+            lParam = reinterpret_cast<LPARAM>(item);
+         }
+
+      public:
+         const ProjectItem*  Data;
+      };
+
       // --------------------- CONSTRUCTION ----------------------
 
    public:
@@ -34,8 +93,7 @@ NAMESPACE_BEGIN2(GUI,Controls)
       void  Populate();
 
    protected:
-      int       GetItemIcon(Logic::Projects::ProjectItem* item);
-      HTREEITEM InsertItem(Logic::Projects::ProjectItem* item, HTREEITEM parent);
+      HTREEITEM InsertItem(TreeItem& item, HTREEITEM parent);
       BOOL      OnNotify(WPARAM wParam, LPARAM lParam, LRESULT* pResult) override;
 
       // -------------------- REPRESENTATION ---------------------
