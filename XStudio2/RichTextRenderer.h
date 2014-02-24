955f715bfa799f455d0dcd5e5ef8e5c9cbcfc117
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-02-24 23:55:46 +0000

    Added proper tooltip colours

--- XStudio2/RichTextRenderer.h
+++ XStudio2/RichTextRenderer.h
@@ -1,7 +1,6 @@
 
 #pragma once
 #include "Logic/RichString.h"
-#include "Logic/RtfStringWriter.h"
 
 namespace GUI
 {
@@ -32,6 +31,37 @@ namespace GUI
             {
                Text.push_back(ch.Char);
             }
+            // ------------------------ STATIC -------------------------
+         private:
+            const static COLORREF  Black = RGB(0,0,0),     White  = RGB(255,255,255), Cyan   = RGB(42,168,200), Blue   = RGB(25,55,242), 
+                                   Green = RGB(13,191,25), Grey   = RGB(128,128,128), Orange = RGB(213,145,28), Purple = RGB(200,36,167), 
+                                   Red   = RGB(180,37,37), Silver = RGB(233,233,233), Yellow = RGB(180,180,0),  Default = RGB(200,200,200);
+
+            /// <summary>Convert colour enumeration to RGB tooltip colour</summary>
+            /// <param name="c">colour</param>
+            /// <returns></returns>
+            static COLORREF ToRGB(Colour c)
+            {
+               switch (c)
+               {
+               case Colour::Black:   return Black;
+               case Colour::Default: return Default;
+               case Colour::White:   return White;
+               case Colour::Silver:  return Silver;
+
+               case Colour::Grey:    return Grey;
+               case Colour::Blue:    return Blue;
+               case Colour::Cyan:    return Cyan;
+               case Colour::Green:   return Green;
+               case Colour::Orange:  return Orange;
+               case Colour::Purple:  return Purple;
+               case Colour::Red:     return Red;
+               case Colour::Yellow:  return Yellow;
+               }
+
+               throw ArgumentException(HERE, L"c", GuiString(L"Unrecognised colour enumeration: %d", c));
+            }
+
             // --------------------- PROPERTIES ------------------------
 
             // ---------------------- ACCESSORS ------------------------			
@@ -54,7 +84,7 @@ namespace GUI
                   }
                
                // Set colour
-               return RtfStringWriter::ToRGB(col);
+               return ToRGB(col);
             }
 
             /// <summary>Creates a new font to represent this phrase</summary>
