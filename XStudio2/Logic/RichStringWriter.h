a818b3ebf7f01003cc637cf8d5831a346eac06d8
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-02-26 17:55:43 +0000

    started new RichStringWriter algorithm...

--- XStudio2/Logic/RichStringWriter.h
+++ XStudio2/Logic/RichStringWriter.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Common.h"
 #include "LanguagePage.h"
+#include "RichStringParser.h"
 
 namespace Logic
 {
@@ -11,7 +12,54 @@ namespace Logic
       class RichStringWriter
       {
          // ------------------------ TYPES --------------------------
-      private:
+      protected:
+         typedef list<TagType>  TagList;
+
+         class TagStack : public list<TagType>
+         {
+         public:
+            TagStack() {}
+
+            void Push(TagType t)
+            {
+               push_front(t);
+            }
+
+            TagType Pop()
+            {
+               auto t = front();
+               pop_front();
+               return t;
+            }
+
+            /*TagStack& operator+=(TagType t)
+            {
+               Push(t);
+               return *this;
+            }*/
+         };
+
+         class CharState
+         {
+         public:
+            CharState() {}
+
+            bool operator==(const CharState& r)
+            {
+               return Bold==r.Bold && Italic==r.Italic && Underline==r.Underline && Colour==r.Colour && Alignment==r.Alignment;
+            }
+            bool operator!=(const CharState& r)
+            {
+               return !operator==(r);
+            }
+
+         public:
+            bool       Bold, 
+                       Italic, 
+                       Underline;
+            Colour     Colour;
+            Alignment  Alignment;
+         };
 
          // --------------------- CONSTRUCTION ----------------------
 
@@ -33,6 +81,11 @@ namespace Logic
          wstring  Write();
 
       protected:
+         void   OnFormattingLost(CharState prev, CharState chr);
+         void   OnFormattingGained(CharState prev, CharState chr);
+         void   OnParagraphOpened(CharState s);
+         void   OnParagraphClosed(CharState s);
+
          Colour GetColour(TextFontPtr& f);
          void   WriteChar(wchar ch);
          void   WriteChar(TextRangePtr chr, TextRangePtr prev);
@@ -43,7 +96,8 @@ namespace Logic
       protected:
          TextDocumentPtr Input;
          wstring         Output;
-         ColourTag       TagType;
+         ColourTag       ColourTags;
+         TagStack        Stack;
       };
 
    }
