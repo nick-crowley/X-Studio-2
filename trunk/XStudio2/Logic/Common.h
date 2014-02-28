a09286aa19440da0f3eec6400fbea968fb518a08
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-02-28 01:26:32 +0000

    Imported RichTextColours from X-Studio 1

--- trunk/XStudio2/Logic/Common.h
+++ trunk/XStudio2/Logic/Common.h
@@ -81,14 +81,13 @@ namespace Logic
       Red,   Silver, Yellow
    };
 
-
    /// <summary>RGB values for RichText colours</summary>
    enum class RichTextColour : COLORREF 
    { 
-      Default = RGB(250,250,250),
-      Black = RGB(0,0,0),   White  = RGB(255,255,255), Blue   = RGB(0,0,255),     Cyan   = RGB(150,220,220), 
-      Green = RGB(0,255,0), Grey   = RGB(128,128,128), Orange = RGB(255,128,40),  Purple = RGB(255,0,255), 
-      Red   = RGB(255,0,0), Silver = RGB(180,180,180), Yellow = RGB(255,255,0) 
+      Default = RGB(200,200,200),
+      Black = RGB(0,0,0),     White  = RGB(255,255,255), Blue   = RGB(0,0,255),     Cyan   = RGB(42,168,200), 
+      Green = RGB(13,191,25), Grey   = RGB(128,128,128), Orange = RGB(255,150,20),  Purple = RGB(210,20,255), 
+      Red   = RGB(204,0,0),   Silver = RGB(233,233,233), Yellow = RGB(215,215,0) 
    };
 
    /// <summary>RGB values for Tooltip colours</summary>
