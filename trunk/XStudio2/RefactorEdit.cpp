71e509da5f4131b9011b07d41667aa9689b6af0d
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-03-11 19:34:51 +0000

    Refactor dialog now highlights symbols using new RefactorEdit

---
+++ trunk/XStudio2/RefactorEdit.cpp
@@ -0,0 +1,139 @@
+#include "stdafx.h"
+#include "RefactorEdit.h"
+
+/// <summary>User interface controls</summary>
+NAMESPACE_BEGIN2(GUI,Controls)
+
+   // --------------------------------- CONSTANTS ----------------------------------
+
+   // --------------------------------- APP WIZARD ---------------------------------
+  
+   IMPLEMENT_DYNAMIC(RefactorEdit, ScriptEdit)
+
+   BEGIN_MESSAGE_MAP(RefactorEdit, ScriptEdit)
+   END_MESSAGE_MAP()
+   
+   // -------------------------------- CONSTRUCTION --------------------------------
+
+   RefactorEdit::RefactorEdit() 
+   {
+   }
+
+   RefactorEdit::~RefactorEdit()
+   {
+   }
+
+   // ------------------------------- STATIC METHODS -------------------------------
+
+   // ------------------------------- PUBLIC METHODS -------------------------------
+
+   /// <summary>Initializes the control</summary>
+   /// <param name="doc">Source script document.</param>
+   /// <exception cref="Logic::ArgumentNullException">document is null</exception>
+   /// <exception cref="Logic::Win32Exception">Unable to retrieve COM pointers</exception>
+   void  RefactorEdit::Initialize(ScriptDocument* doc)
+   {
+      // Set ReadOnly
+      ReadOnly = TRUE;
+
+      // Initialize
+      __super::Initialize(doc);
+   }
+
+   /// <summary>Highlights/Unhighlights a single symbol.</summary>
+   /// <param name="s">symbol.</param>
+   /// <param name="highlight">symbol.</param>
+   void RefactorEdit::HighlightSymbol(const Symbol& s, bool highlight)
+   {
+      // Freeze
+      SuspendUndo(true);
+      FreezeWindow(true);
+
+      // Define normal/refactor highlight
+      CharFormat cf(CFM_BACKCOLOR, 0);
+      cf.crBackColor = (highlight ? RGB(128,128,128) : PrefsLib.BackgroundColour);
+
+      // Highlight
+      UINT offset = LineIndex(s.LineNumber-1);
+      SetSel(offset+s.Token.Start, offset+s.Token.End);
+      SetSelectionCharFormat(cf);
+
+      // Restore 
+      FreezeWindow(false);
+      SuspendUndo(false);
+   }
+
+   /// <summary>Highlights all symbols.</summary>
+   /// <param name="symbols">The symbols.</param>
+   void RefactorEdit::HighlightSymbols(const vector<Symbol> symbols)
+   {
+      // Freeze
+      SuspendUndo(true);
+      FreezeWindow(true);
+
+      // Define refactor highlight
+      CharFormat cf(CFM_BACKCOLOR, 0);
+      cf.crBackColor = RGB(128,128,128);
+
+      // Highlight symbols
+      for (auto& s : symbols)
+      {
+         UINT offset = LineIndex(s.LineNumber-1);
+
+         // Highlight
+         SetSel(offset+s.Token.Start, offset+s.Token.End);
+         SetSelectionCharFormat(cf);
+      }
+
+      // Restore 
+      FreezeWindow(false);
+      SuspendUndo(false);
+   }
+   
+   /// <summary>Scrolls edit to display a symbol</summary>
+   /// <param name="s">symbol.</param>
+   void  RefactorEdit::ScrollTo(const Symbol& s)
+   {
+      // Scroll to a few lines before symbol
+      EnsureVisible( s.LineNumber-4 );
+   }
+
+   // ------------------------------ PROTECTED METHODS -----------------------------
+   
+   /// <summary>Updates the highlighting.</summary>
+   /// <param name="first">first zero-based line number</param>
+   /// <param name="last">last zero-based line number.</param>
+   //void RefactorEdit::UpdateHighlighting(int first, int last)
+   //{
+   //   // Perform syntax colouring
+   //   __super::UpdateHighlighting(first, last);
+
+   //   // Freeze window
+   //   SuspendUndo(true);
+   //   FreezeWindow(true);
+
+   //   // Define grey background
+   //   CharFormat cf(CFM_BACKCOLOR,0);
+   //   cf.crBackColor = RGB(80,80,80);
+
+   //   // Highlight phrases
+   //   for (auto p : DiffDocument->Phrases)
+   //   {
+   //      SetSel(p.GetStart(), p.GetEnd());
+   //      SetSelectionCharFormat(cf);
+   //   }
+
+   //   // Restore selection
+   //   FreezeWindow(false);
+   //   SuspendUndo(false);
+   //}
+
+   // ------------------------------- PRIVATE METHODS ------------------------------
+   
+
+
+   
+NAMESPACE_END2(GUI,Controls)
+
+
+
