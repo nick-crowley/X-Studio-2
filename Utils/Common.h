859a8279ae750e3e0ad911d5f58914b308014163
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-03-16 15:56:14 +0000

    Merged the utils and common headers

--- Utils/Common.h
+++ Utils/Common.h
@@ -1,94 +1,113 @@
 #pragma once
 #include "Macros.h"
+#include "Types.h"
+//#include "Utils.h"
+#include "Path.h"
+#include "Exceptions.h"
+#include "GuiString.h"
+#include "ConsoleWnd.h"
 
-/// <summary>A unique pointer to a byte array</summary>
-typedef unique_ptr<BYTE, std::default_delete<BYTE[]>>  ByteArrayPtr;
 
-/// <summary>A unique pointer to a wide char array</summary>
-typedef unique_ptr<WCHAR, std::default_delete<WCHAR[]>>  CharArrayPtr;
-
-/// <summary>Wide char</summary>
-typedef wchar_t wchar;
-
-/// <summary>Vector of strings</summary>
-typedef vector<wstring>  LineArray;
-
-/// <summary>Constant line iterator</summary>
-typedef LineArray::const_iterator  LineIterator;
-
-#include <Richedit.h>
-
+/// <summary>Logic</summary>
 namespace Logic
 {
-   
-   /// <summary>Paragraph alignment</summary>
-   enum class Alignment { Left = PFA_LEFT, Centre = PFA_CENTER, Right = PFA_RIGHT, Justify = PFA_FULL_INTERWORD };
-
-   /// <summary>Get paragraph alignment string</summary>
-   wstring  GetString(Alignment a);
-
-   /// <summary>Device independent colour enumeration</summary>
-   enum class Colour  
-   { 
-      Default,
-      Black, White,  Blue,   Cyan,
-      Green, Grey,   Orange, Purple,
-      Red,   Silver, Yellow
-   };
-
-   /// <summary>RGB values for RichText colours</summary>
-   enum class RichTextColour : COLORREF 
-   { 
-      Default = RGB(200,200,200),
-      Black = RGB(0,0,0),     White  = RGB(255,255,255), Blue   = RGB(0,0,255),     Cyan   = RGB(42,168,200), 
-      Green = RGB(13,191,25), Grey   = RGB(128,128,128), Orange = RGB(255,150,20),  Purple = RGB(210,20,255), 
-      Red   = RGB(204,0,0),   Silver = RGB(233,233,233), Yellow = RGB(215,215,0) 
-   };
-
-   /// <summary>RGB values for Tooltip colours</summary>
-   enum class TooltipColour : COLORREF
-   {
-      Default = RGB(200,200,200),
-      Black = RGB(0,0,0),     White  = RGB(255,255,255), Blue   = RGB(25,55,242),  Cyan   = RGB(42,168,200), 
-      Green = RGB(13,191,25), Grey   = RGB(128,128,128), Orange = RGB(213,145,28), Purple = RGB(200,36,167), 
-      Red   = RGB(180,37,37), Silver = RGB(233,233,233), Yellow = RGB(180,180,0)
-   };
-
    /// <summary>Convert colour enumeration to RGB</summary>
    RichTextColour ForRichText(Colour c);
 
    /// <summary>Convert colour enumeration to RGB</summary>
    TooltipColour ForTooltip(Colour c);
 
-   /// <summary>Compare character ranges</summary>
-   bool operator==(const CHARRANGE& a, const CHARRANGE& b);
-   bool operator!=(const CHARRANGE& a, const CHARRANGE& b);
-
-
-   template<typename T> 
-   class reverse_adapter 
-   { 
-   public: 
-      reverse_adapter(T& c) : c(c) 
-      {} 
-      
-      typename T::reverse_iterator begin() { return c.rbegin(); } 
-      typename T::reverse_iterator end()   { return c.rend();   } 
-
-      typename T::const_reverse_iterator begin() const { return c.rbegin(); } 
-      typename T::const_reverse_iterator end() const   { return c.rend();   } 
-   
-   private: 
-      T& c; 
-   };
-
-
-   /// <summary>Defines the available game data languages</summary>
-   enum class GameLanguage : UINT { Russian = 7, French = 33, Spanish = 34, Italian = 39, Czech = 42, English = 44, Polish = 48, German = 49 };
+   /// <summary>Get paragraph alignment string</summary>
+   wstring  GetString(Alignment a);
 
    /// <summary>Get game language string</summary>
    wstring  GetString(GameLanguage l);
 
+   /// <summary>Compare character ranges</summary>
+   bool operator==(const CHARRANGE& a, const CHARRANGE& b);
+   bool operator!=(const CHARRANGE& a, const CHARRANGE& b);
+
+   /// <summary>Write rectangle to the console</summary>
+   ConsoleWnd& operator<<(ConsoleWnd& c, const CRect& rc);
+
+   /// <summary>Write size to the console</summary>
+   ConsoleWnd& operator<<(ConsoleWnd& c, const CSize& sz);
+
+   /// <summary>Dialog data exchange for std::wstring</summary>
+   void AFXAPI DDX_Text(CDataExchange* pDX, int nIDC, wstring& value);
+
+   /// <summary>Dialog data exchange for paths</summary>
+   void AFXAPI DDX_Text(CDataExchange* pDX, int nIDC, Path& path);
+
+
+   /// <summary>Client rectangle</summary>
+   class ClientRect : public CRect
+   {
+      // --------------------- CONSTRUCTION ----------------------
+   public:
+      ClientRect(CWnd* wnd) : CRect(0,0,0,0)
+      {
+         wnd->GetClientRect(this);
+      }
+
+      DEFAULT_COPY(ClientRect);	// Default copy semantics
+      DEFAULT_MOVE(ClientRect);	// Default move semantics
+
+      // -------------------- REPRESENTATION ---------------------
+   };
+
+   /// <summary>Control rectangle in parent client co-ordinates</summary>
+   class CtrlRect : public CRect
+   {
+      // --------------------- CONSTRUCTION ----------------------
+   public:
+      /// <summary>Create control rectangle in parent client co-ordinates</summary>
+      /// <param name="parent">The parent.</param>
+      /// <param name="ctrl">The control.</param>
+      CtrlRect(CWnd* parent, CWnd* ctrl) : CRect(0, 0, 0, 0)
+      {
+         ctrl->GetWindowRect(this);
+         parent->ScreenToClient(this);
+      }
+
+      DEFAULT_COPY(CtrlRect);	// Default copy semantics
+      DEFAULT_MOVE(CtrlRect);	// Default move semantics
+
+   };
+
+   /// <summary>Window rectangle</summary>
+   class WindowRect : public CRect
+   {
+      // --------------------- CONSTRUCTION ----------------------
+   public:
+      WindowRect(CWnd* wnd) : CRect(0,0,0,0)
+      {
+         wnd->GetWindowRect(this);
+      }
+
+      DEFAULT_COPY(WindowRect);	// Default copy semantics
+      DEFAULT_MOVE(WindowRect);	// Default move semantics
+
+      // -------------------- REPRESENTATION ---------------------
+   };
+
+
+   /// <summary>Position of mouse cursor</summary>
+   class CursorPoint : public CPoint
+   {
+   public:
+      /// <summary>Create cursor position.</summary>
+      /// <param name="wnd">Window for client co=ordinates, or nullptr for screen co-ords</param>
+      CursorPoint(const CWnd* wnd = nullptr) : CPoint(0, 0)
+      {
+         // Get position in screen co-ords
+         if (GetCursorPos(this) && wnd)
+            // Window: Convert to client co-ords
+            ScreenToClient(*wnd, this);
+      }
+   };
+
+   
    /// <summary>Map Game Language into zero-based index</summary>
    class GameLanguageIndex
    {
@@ -131,11 +150,6 @@ namespace Logic
       GameLanguage  Language;
    };
 
-
-
-   /// <summary>Defines the available game versions</summary>
-   enum class GameVersion : UINT { Threat=1, Reunion=2, TerranConflict=4, AlbionPrelude=8, Rebirth=16 };
-
    /// <summary>Map Game version into zero-based index</summary>
    class GameVersionIndex
    {
@@ -172,15 +186,30 @@ namespace Logic
       GameVersion  Version;
    };
 
+   
+   /// <summary>Reverse iterator adapater</summary>
+   /*template<typename T> 
+   class reverse_adapter 
+   { 
+   public: 
+      reverse_adapter(T& c) : c(c) 
+      {} 
+      
+      typename T::reverse_iterator begin() { return c.rbegin(); } 
+      typename T::reverse_iterator end()   { return c.rend();   } 
+
+      typename T::const_reverse_iterator begin() const { return c.rbegin(); } 
+      typename T::const_reverse_iterator end() const   { return c.rend();   } 
+   
+   private: 
+      T& c; 
+   };*/
+
+
+
+   
+
+   
 }
 
-using namespace Logic;
-
-
-
-#include "Path.h"
-#include "Exceptions.h"
-#include "GuiString.h"
-#include "ConsoleWnd.h"
-#include "Utils.h"
 
