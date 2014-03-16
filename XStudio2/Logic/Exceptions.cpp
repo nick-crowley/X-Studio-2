47f168796eb6484975c6a50791d9717a2e4f6f05
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-03-16 14:21:25 +0000

    separated exception header/implementation

---
+++ XStudio2/Logic/Exceptions.cpp
@@ -0,0 +1,235 @@
+#pragma once
+#include "stdafx.h"
+#include "Exceptions.h"
+#include "../../Resources/Resources.h"
+#include "Path.h"
+
+namespace Logic
+{
+   // -------------------------------- CONSTRUCTION --------------------------------
+   
+   /// <summary>Create a exception without an error code but with a message</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="msg">Error message</param>
+   ExceptionBase::ExceptionBase(wstring  src, wstring msg) : ErrorID(0), Message(msg), Source(src)
+   {}
+
+   /// <summary>Create a exception with an error code and message</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="err">Error id</param>
+   /// <param name="msg">Error message</param>
+   ExceptionBase::ExceptionBase(wstring  src, UINT  err, wstring msg) : ErrorID(err), Message(msg), Source(src)
+   {}
+
+   
+   ExceptionBase::~ExceptionBase()
+   {}
+   
+   /// <summary>Create an AlgorithmException</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="msg">Message</param>
+   AlgorithmException::AlgorithmException(wstring  src, wstring  msg) 
+      : ExceptionBase(src, wstring(L"Invariant violated: ") + msg)
+   {}
+   
+   /// <summary>Create an ApplicationException</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="msg">Message</param>
+   ApplicationException::ApplicationException(wstring  src, wstring  msg) 
+      : ExceptionBase(src, msg)
+   {}
+
+   /// <summary>Convert existing exception into an ApplicationException</summary>
+   /// <param name="r">Exception</param>
+   ApplicationException::ApplicationException(const ExceptionBase& r) 
+      : ExceptionBase(r.Source, r.Message)
+   {}
+   
+   /// <summary>Create an ArgumentNullException</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="arg">Name of argument</param>
+   ArgumentNullException::ArgumentNullException(wstring  src, const WCHAR* arg) 
+      // "Missing '%s' argument"
+      : ExceptionBase(src, VString(ERR_ARGUMENT_NULL, arg))
+   {}
+
+   /// <summary>Create an ArgumentException</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="arg">Name of argument</param>
+   /// <param name="msg">Message</param>
+   ArgumentException::ArgumentException(wstring  src, wstring  arg, wstring  msg) 
+      // "The '%s' argument is invalid : %s"
+      : ExceptionBase(src, VString(L"The '%s' argument is invalid : %s", arg.c_str(), msg.c_str()) )
+   {}
+   
+   /// <summary>Create a ComException from a _com_error</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="err">COM error</param>
+   ComException::ComException(wstring  src, _com_error&  err) 
+      // "COM object error: ...."
+      : ExceptionBase(src, err.Error(), wstring(L"COM object error: ") + err.ErrorMessage())
+   {}
+
+   /// <summary>Create a ComException from a _com_error</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="err">COM error</param>
+   ComException::ComException(wstring  src, HRESULT hr) 
+      // "COM object error: ...."
+      : ExceptionBase(src, hr, wstring(L"COM object error: ") + SysErrorString(hr))
+   {}
+
+   /// <summary>Create a DirectoryNotFoundException from a folder path</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="file">Folder path</param>
+   DirectoryNotFoundException::DirectoryNotFoundException(wstring  src, IO::Path  folder) 
+      // "The folder '%s' does not exist"
+      : ExceptionBase(src, ERROR_PATH_NOT_FOUND, VString(L"The folder '%s' does not exist", folder.c_str()) )
+   {}
+
+
+   /// <summary>Create an FileFormatException</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="msg">Message</param>
+   FileFormatException::FileFormatException(wstring  src, wstring  msg) 
+      : ExceptionBase(src, wstring(L"The file format is invalid: ") + msg)
+   {}
+
+   /// <summary>Create an FileFormatException for a specific line</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="line">One based line number</param>
+   /// <param name="msg">Message</param>
+   FileFormatException::FileFormatException(wstring  src, UINT line, wstring  msg) 
+      : ExceptionBase(src, VString(L"The file format is invalid on line %u: ", line) + msg)
+   {}
+
+   /// <summary>Create a FileNotFoundException for a file path</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="file">File path</param>
+   FileNotFoundException::FileNotFoundException(wstring  src, IO::Path  file) 
+      : ExceptionBase(src, ERROR_FILE_NOT_FOUND, VString(L"The file '%s' does not exist", file.c_str()))
+   {}
+   
+   /// <summary>Create a GZipException from a custom message</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="msg">Custom message</param>
+   GZipException::GZipException(wstring  src, const wstring&  msg) 
+      : ExceptionBase(src, L"Error in gzip handling: " + msg)
+   {}
+
+   /// <summary>Create a GZipException from a zlib message</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="msg">ZLib message</param>
+   GZipException::GZipException(wstring  src, const char*  msg) 
+      : ExceptionBase(src, L"Error in zlib library: ")
+   {
+      if (msg)
+      {
+         string sz(msg);
+         Message += wstring(sz.begin(), sz.end());
+      }
+      else
+         Message += L"Unspecified";
+   }
+   
+   /// <summary>Create an IndexOutOfRangeException</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="index">Desired index</param>
+   /// <param name="count">Item count</param>
+   IndexOutOfRangeException::IndexOutOfRangeException(wstring  src, UINT index, UINT count) 
+      : ExceptionBase(src, VString(L"The index %d is out of range, only %d objects in collection", index, count))
+   {}
+
+   /// <summary>Create an IndexOutOfRangeException</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="index">Desired index</param>
+   IndexOutOfRangeException::IndexOutOfRangeException(wstring  src, UINT index) 
+      : ExceptionBase(src, VString(L"The index %d is out of range", index))
+   {}
+
+   /// <summary>Create an InvalidOperationException</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="msg">Message</param>
+   InvalidOperationException::InvalidOperationException(wstring  src, wstring  msg) 
+      : ExceptionBase(src, wstring(L"The operation is invalid: ") + msg)
+   {}
+
+   /// <summary>Create an InvalidValueException</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="msg">Message</param>
+   InvalidValueException::InvalidValueException(wstring  src, wstring  msg) 
+      : ExceptionBase(src, wstring(L"Invalid game data value: ") + msg)
+   {}
+
+   /// <summary>Create an IOException</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="msg">Message</param>
+   IOException::IOException(wstring  src, wstring  msg) 
+      : ExceptionBase(src, wstring(L"The I/O operation failed: ") + msg)
+   {}
+
+   /// <summary>Create a NotImplementedException</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="msg">message</param>
+   NotImplementedException::NotImplementedException(wstring  src, wstring  msg) 
+      : ExceptionBase(src, wstring(L"The operation has not been implemented: ") + msg)
+   {}
+
+   /// <summary>Create an NotSupportedException</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="msg">message</param>
+   NotSupportedException::NotSupportedException(wstring  src, wstring  msg) 
+      : ExceptionBase(src, wstring(L"The operation is invalid: ") + msg)
+   {}
+
+
+   /// <summary>Create an Win32Exception from the last system error</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="msg">message</param>
+   Win32Exception::Win32Exception(wstring  src, wstring  msg) 
+      : ExceptionBase(src, msg + L" : " + (GetLastError() ? (wstring)SysErrorString() : L"<Subsystem provided no further details>"))
+   {}
+
+   
+   // ------------------------ USER TYPES --------------------------
+
+
+   /// <summary>Create a GenericException</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="msg">Message</param>
+   GenericException::GenericException(wstring  src, wstring msg) 
+      : ExceptionBase(src, msg)
+   {}
+
+
+   /// <summary>Create a PageNotFoundException</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="page">page id</param>
+   PageNotFoundException::PageNotFoundException(wstring  src, UINT page) 
+      : ExceptionBase(src, VString(L"Cannot find page %d", page))
+   {}
+
+   /// <summary>Create a StringNotFoundException</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="page">page id</param>
+   /// <param name="id">string id</param>
+   StringNotFoundException::StringNotFoundException(wstring  src, UINT page, UINT id) 
+      : ExceptionBase(src, VString(L"Cannot find string %d in page %d", id, page))
+   {}
+
+   /// <summary>Create an SyntaxNotFoundException</summary>
+   /// <param name="src">Location of throw</param>
+   /// <param name="id">command id</param>
+   /// <param name="ver">game version</param>
+   SyntaxNotFoundException::SyntaxNotFoundException(wstring  src, UINT id, GameVersion v) 
+      : ExceptionBase(src, VString(L"Cannot find %s syntax for command with id %d", VersionString(v).c_str(), id))
+   {}
+
+   
+   // ------------------------------- STATIC METHODS -------------------------------
+
+   // ------------------------------- PUBLIC METHODS -------------------------------
+
+   // ------------------------------ PROTECTED METHODS -----------------------------
+
+   // ------------------------------- PRIVATE METHODS ------------------------------  
+}
--- XStudio2/Logic/Exceptions.h
+++ XStudio2/Logic/Exceptions.h
@@ -1,6 +1,4 @@
 #pragma once
-#include "D:/My Projects/XStudio2/Resources/Resources.h"
-
 #include "GuiString.h"
 #include "Path.h"
 
@@ -10,22 +8,19 @@ namespace Logic
    class ExceptionBase : public std::exception
    {
    protected:
-      
       /// <summary>Create a exception without an error code but with a message</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="msg">Error message</param>
-      ExceptionBase(wstring  src, wstring msg) : ErrorID(0), Message(msg), Source(src)
-      {}
+      ExceptionBase(wstring  src, wstring msg);
 
       /// <summary>Create a exception with an error code and message</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="err">Error id</param>
       /// <param name="msg">Error message</param>
-      ExceptionBase(wstring  src, UINT  err, wstring msg) : ErrorID(err), Message(msg), Source(src)
-      {}
+      ExceptionBase(wstring  src, UINT  err, wstring msg);
 
    public:
-      virtual ~ExceptionBase()  {}
+      virtual ~ExceptionBase();
 
       UINT      ErrorID;
       GuiString Message,
@@ -39,9 +34,7 @@ namespace Logic
       /// <summary>Create an AlgorithmException</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="msg">Message</param>
-      AlgorithmException(wstring  src, wstring  msg) 
-         : ExceptionBase(src, wstring(L"Invariant violated: ") + msg)
-      {}
+      AlgorithmException(wstring  src, wstring  msg);
    };
    
    /// <summary>Occurs when an error should be displayed to the user verbatim</summary>
@@ -51,15 +44,11 @@ namespace Logic
       /// <summary>Create an ApplicationException</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="msg">Message</param>
-      ApplicationException(wstring  src, wstring  msg) 
-         : ExceptionBase(src, msg)
-      {}
+      ApplicationException(wstring  src, wstring  msg);
 
       /// <summary>Convert existing exception into an ApplicationException</summary>
       /// <param name="r">Exception</param>
-      ApplicationException(const ExceptionBase& r) 
-         : ExceptionBase(r.Source, r.Message)
-      {}
+      ApplicationException(const ExceptionBase& r) ;
    };
    
    /// <summary>Occurs when a missing argument is detected</summary>
@@ -69,10 +58,7 @@ namespace Logic
       /// <summary>Create an ArgumentNullException</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="arg">Name of argument</param>
-      ArgumentNullException(wstring  src, const WCHAR* arg) 
-         // "Missing '%s' argument"
-         : ExceptionBase(src, VString(ERR_ARGUMENT_NULL, arg))
-      {}
+      ArgumentNullException(wstring  src, const WCHAR* arg);
    };
 
    /// <summary>Occurs when an argument is invalid</summary>
@@ -83,10 +69,7 @@ namespace Logic
       /// <param name="src">Location of throw</param>
       /// <param name="arg">Name of argument</param>
       /// <param name="msg">Message</param>
-      ArgumentException(wstring  src, wstring  arg, wstring  msg) 
-         // "The '%s' argument is invalid : %s"
-         : ExceptionBase(src, VString(L"The '%s' argument is invalid : %s", arg.c_str(), msg.c_str()) )
-      {}
+      ArgumentException(wstring  src, wstring  arg, wstring  msg);
    };
    
    /// <summary>Occurs when a COM object is used incorrectly</summary>
@@ -96,18 +79,12 @@ namespace Logic
       /// <summary>Create a ComException from a _com_error</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="err">COM error</param>
-      ComException(wstring  src, _com_error&  err) 
-         // "COM object error: ...."
-         : ExceptionBase(src, err.Error(), wstring(L"COM object error: ") + err.ErrorMessage())
-      {}
+      ComException(wstring  src, _com_error&  err);
 
       /// <summary>Create a ComException from a _com_error</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="err">COM error</param>
-      ComException(wstring  src, HRESULT hr) 
-         // "COM object error: ...."
-         : ExceptionBase(src, hr, wstring(L"COM object error: ") + SysErrorString(hr))
-      {}
+      ComException(wstring  src, HRESULT hr);
    };
 
    /// <summary>Occurs when a file is not found</summary>
@@ -117,10 +94,7 @@ namespace Logic
       /// <summary>Create a DirectoryNotFoundException from a folder path</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="file">Folder path</param>
-      DirectoryNotFoundException(wstring  src, IO::Path  folder) 
-         // "The folder '%s' does not exist"
-         : ExceptionBase(src, ERROR_PATH_NOT_FOUND, VString(L"The folder '%s' does not exist", folder.c_str()) )
-      {}
+      DirectoryNotFoundException(wstring  src, IO::Path  folder);
    };
 
 
@@ -131,17 +105,13 @@ namespace Logic
       /// <summary>Create an FileFormatException</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="msg">Message</param>
-      FileFormatException(wstring  src, wstring  msg) 
-         : ExceptionBase(src, wstring(L"The file format is invalid: ") + msg)
-      {}
+      FileFormatException(wstring  src, wstring  msg);
 
       /// <summary>Create an FileFormatException for a specific line</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="line">One based line number</param>
       /// <param name="msg">Message</param>
-      FileFormatException(wstring  src, UINT line, wstring  msg) 
-         : ExceptionBase(src, VString(L"The file format is invalid on line %u: ", line) + msg)
-      {}
+      FileFormatException(wstring  src, UINT line, wstring  msg);
    };
 
    /// <summary>Occurs when a file is not found</summary>
@@ -151,9 +121,7 @@ namespace Logic
       /// <summary>Create a FileNotFoundException for a file path</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="file">File path</param>
-      FileNotFoundException(wstring  src, IO::Path  file) 
-         : ExceptionBase(src, ERROR_FILE_NOT_FOUND, VString(L"The file '%s' does not exist", file.c_str()))
-      {}
+      FileNotFoundException(wstring  src, IO::Path  file);
    };
    
    /// <summary>Occurs when zlib encounters an error</summary>
@@ -163,24 +131,12 @@ namespace Logic
       /// <summary>Create a GZipException from a custom message</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="msg">Custom message</param>
-      GZipException(wstring  src, const wstring&  msg) 
-         : ExceptionBase(src, L"Error in gzip handling: " + msg)
-      {}
+      GZipException(wstring  src, const wstring&  msg);
 
       /// <summary>Create a GZipException from a zlib message</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="msg">ZLib message</param>
-      GZipException(wstring  src, const char*  msg) 
-         : ExceptionBase(src, L"Error in zlib library: ")
-      {
-         if (msg)
-         {
-            string sz(msg);
-            Message += wstring(sz.begin(), sz.end());
-         }
-         else
-            Message += L"Unspecified";
-      }
+      GZipException(wstring  src, const char*  msg);
    };
    
    /// <summary>Occurs when an index is out of range</summary>
@@ -191,16 +147,12 @@ namespace Logic
       /// <param name="src">Location of throw</param>
       /// <param name="index">Desired index</param>
       /// <param name="count">Item count</param>
-      IndexOutOfRangeException(wstring  src, UINT index, UINT count) 
-         : ExceptionBase(src, VString(L"The index %d is out of range, only %d objects in collection", index, count))
-      {}
+      IndexOutOfRangeException(wstring  src, UINT index, UINT count);
 
       /// <summary>Create an IndexOutOfRangeException</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="index">Desired index</param>
-      IndexOutOfRangeException(wstring  src, UINT index) 
-         : ExceptionBase(src, VString(L"The index %d is out of range", index))
-      {}
+      IndexOutOfRangeException(wstring  src, UINT index);
    };
 
    /// <summary>Occurs when an invalid operation is attempted</summary>
@@ -210,9 +162,7 @@ namespace Logic
       /// <summary>Create an InvalidOperationException</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="msg">Message</param>
-      InvalidOperationException(wstring  src, wstring  msg) 
-         : ExceptionBase(src, wstring(L"The operation is invalid: ") + msg)
-      {}
+      InvalidOperationException(wstring  src, wstring  msg);
    };
 
    /// <summary>Occurs when an invalid value is detected within game data</summary>
@@ -222,9 +172,7 @@ namespace Logic
       /// <summary>Create an InvalidValueException</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="msg">Message</param>
-      InvalidValueException(wstring  src, wstring  msg) 
-         : ExceptionBase(src, wstring(L"Invalid game data value: ") + msg)
-      {}
+      InvalidValueException(wstring  src, wstring  msg);
    };
 
    /// <summary>Occurs when an I/O operator fails</summary>
@@ -234,9 +182,7 @@ namespace Logic
       /// <summary>Create an IOException</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="msg">Message</param>
-      IOException(wstring  src, wstring  msg) 
-         : ExceptionBase(src, wstring(L"The I/O operation failed: ") + msg)
-      {}
+      IOException(wstring  src, wstring  msg);
    };
 
    /// <summary>Occurs when an operation that has not been implemented is attempted</summary>
@@ -246,9 +192,7 @@ namespace Logic
       /// <summary>Create a NotImplementedException</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="msg">message</param>
-      NotImplementedException(wstring  src, wstring  msg) 
-         : ExceptionBase(src, wstring(L"The operation has not been implemented: ") + msg)
-      {}
+      NotImplementedException(wstring  src, wstring  msg);
    };
 
    /// <summary>Occurs when an unsupported operation is attempted</summary>
@@ -258,9 +202,7 @@ namespace Logic
       /// <summary>Create an NotSupportedException</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="msg">message</param>
-      NotSupportedException(wstring  src, wstring  msg) 
-         : ExceptionBase(src, wstring(L"The operation is invalid: ") + msg)
-      {}
+      NotSupportedException(wstring  src, wstring  msg) ;
    };
 
 
@@ -271,9 +213,7 @@ namespace Logic
       /// <summary>Create an Win32Exception from the last system error</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="msg">message</param>
-      Win32Exception(wstring  src, wstring  msg) 
-         : ExceptionBase(src, msg + L" : " + (GetLastError() ? (wstring)SysErrorString() : L"<Subsystem provided no further details>"))
-      {}
+      Win32Exception(wstring  src, wstring  msg);
    };
 
    
@@ -287,9 +227,7 @@ namespace Logic
       /// <summary>Create a GenericException</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="msg">Message</param>
-      GenericException(wstring  src, wstring msg) 
-         : ExceptionBase(src, msg)
-      {}
+      GenericException(wstring  src, wstring msg);
    };
 
 
@@ -300,9 +238,7 @@ namespace Logic
       /// <summary>Create a PageNotFoundException</summary>
       /// <param name="src">Location of throw</param>
       /// <param name="page">page id</param>
-      PageNotFoundException(wstring  src, UINT page) 
-         : ExceptionBase(src, VString(L"Cannot find page %d", page))
-      {}
+      PageNotFoundException(wstring  src, UINT page);
    };
 
    /// <summary>Occurs when a language string is missing</summary>
@@ -313,9 +249,7 @@ namespace Logic
       /// <param name="src">Location of throw</param>
       /// <param name="page">page id</param>
       /// <param name="id">string id</param>
-      StringNotFoundException(wstring  src, UINT page, UINT id) 
-         : ExceptionBase(src, VString(L"Cannot find string %d in page %d", id, page))
-      {}
+      StringNotFoundException(wstring  src, UINT page, UINT id);
    };
 
    /// <summary>Occurs when a command syntax is missing</summary>
@@ -326,9 +260,7 @@ namespace Logic
       /// <param name="src">Location of throw</param>
       /// <param name="id">command id</param>
       /// <param name="ver">game version</param>
-      SyntaxNotFoundException(wstring  src, UINT id, GameVersion v) 
-         : ExceptionBase(src, VString(L"Cannot find %s syntax for command with id %d", VersionString(v).c_str(), id))
-      {}
+      SyntaxNotFoundException(wstring  src, UINT id, GameVersion v);
    };
 
    
