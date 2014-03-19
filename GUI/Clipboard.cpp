e985e737f685a703dc15207b5e405169f9ccbe0c
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-03-19 21:15:30 +0000

    Added: LanguageString XML is copied to clipboard, and plain text pasted into ScriptEdit

--- GUI/Clipboard.cpp
+++ GUI/Clipboard.cpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "stdafx.h"
 #include "Clipboard.h"
-
+#include <strsafe.h>
 
 namespace GUI
 {
@@ -11,14 +11,58 @@ namespace GUI
       Clipboard  Clipboard::Instance;
 
       /// <summary>Clipboard data</summary>
-      Clipboard::LanguageStringPtr  Clipboard::StringData;
-      Clipboard::LanguagePagePtr    Clipboard::PageData;
+      /*Clipboard::LanguageStringPtr  Clipboard::StringData;
+      Clipboard::LanguagePagePtr    Clipboard::PageData;*/
 
       // -------------------------------- CONSTRUCTION --------------------------------
+
+      Clipboard::Clipboard() : Content(Content)
+      {}
+
+      Clipboard::~Clipboard() 
+      {
+         Free();
+      }
 
       // ------------------------------- STATIC METHODS -------------------------------
 
       // ------------------------------- PUBLIC METHODS -------------------------------
+
+      /// <summary>Copies a string to the clipboard.</summary>
+      /// <param name="txt">The text.</param>
+      void  Clipboard::SetString(const wstring& txt)
+      {
+         wchar* buffer = nullptr;
+
+         try
+         {
+            // Open clipboard
+            if (!OpenClipboard(theApp.m_pMainWnd->m_hWnd))
+               throw Win32Exception(HERE, L"Unable to open clipboard");
+
+            // Allocate+Open buffer
+            if ( !(Content = GlobalAlloc(GMEM_MOVEABLE, (txt.length()+1)*sizeof(wchar))) || !(buffer = (wchar*)GlobalLock(Content)) )
+               throw Win32Exception(HERE, L"Unable to allocate global memory");
+
+            // Copy output to the clipboard buffer
+            StringCchCopy(buffer, txt.length()+1, txt.c_str());
+            GlobalUnlock(Content);
+               
+            // Transfer to clipboard
+            if (!SetClipboardData(CF_UNICODETEXT, Content))
+               throw Win32Exception(HERE, L"Unable to set clipboard data");
+
+            // Close
+            CloseClipboard();
+         }
+         catch (...) 
+         {
+            // Cleanup before exit
+            Free();
+            CloseClipboard();
+            throw;
+         }
+      }
 
       // ------------------------------ PROTECTED METHODS -----------------------------
 
