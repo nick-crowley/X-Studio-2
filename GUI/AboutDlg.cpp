fe49e683c2b9b76c75bdb7cc291aa28a0e1ee9f5
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-03-25 16:11:56 +0000

    Added 'About' dialog

--- GUI/AboutDlg.cpp
+++ GUI/AboutDlg.cpp
@@ -19,15 +19,34 @@ NAMESPACE_BEGIN2(GUI,Windows)
 
    // ------------------------------- PUBLIC METHODS -------------------------------
 
+   /// <summary>initialize dialog.</summary>
+   /// <returns></returns>
+   BOOL CAboutDlg::OnInitDialog()
+   {
+      __super::OnInitDialog();
+
+      // Title: About X-Studio II
+      auto title = VString(L"About %s", GuiString(AFX_IDS_APP_TITLE).c_str());
+      SetWindowText(title.c_str());
+
+      // Version: X-Studio II  (v1.0)
+      auto version = VString(L"%s (%s)", GuiString(AFX_IDS_APP_TITLE).c_str(), BUILD_NAME);
+      SetDlgItemText(IDC_NAME_STATIC, version.c_str());
+
+      // Display
+      CenterWindow();
+      return TRUE;
+   }
 
    // ------------------------------ PROTECTED METHODS -----------------------------
 
    void CAboutDlg::DoDataExchange(CDataExchange* pDX)
    {
-	   CDialogEx::DoDataExchange(pDX);
+	   __super::DoDataExchange(pDX);
    }
 
    // ------------------------------- PRIVATE METHODS ------------------------------
 
 
 NAMESPACE_END2(GUI,Windows)
+
