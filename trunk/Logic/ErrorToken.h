8e1e9a2b6a8211ac569e1e049d7c700219b1c4c5
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-03-26 17:16:58 +0000

    Separated ErrorToken/Symbol into separate header from CommandTree

---
+++ trunk/Logic/ErrorToken.h
@@ -0,0 +1,78 @@
+#pragma once
+#include "ScriptToken.h"
+
+namespace Logic
+{
+   namespace Scripts
+   {
+      namespace Compiler
+      {
+         /// <summary>Script error</summary>
+         class LogicExport ErrorToken : public TokenBase
+         {
+            // --------------------- CONSTRUCTION ----------------------
+         public:
+#ifdef LOGIC_COMPILER_FIX
+            ErrorToken() : Line(0) 
+               { throw AlgorithmException(HERE, L"LogicLibrary compiler-fix code executed"); }
+#endif
+            /// <summary>Create error for token</summary>
+            ErrorToken(const GuiString& msg, UINT line, const ScriptToken& tok) 
+               : TokenBase(tok), Message(msg), Text(tok.Text), Line(line)
+            {}
+            /// <summary>Create error for a character range</summary>
+            ErrorToken(const GuiString& msg, UINT line, const GuiString& txt, const CHARRANGE& r) 
+               : TokenBase(r.cpMin, r.cpMax), Message(msg), Text(txt), Line(line)
+            {}
+            /// <summary>Create error for a character range</summary>
+            ErrorToken(const GuiString& msg, UINT line, const GuiString& txt, UINT start, UINT end) 
+               : TokenBase(start,end), Message(msg), Text(txt), Line(line)
+            {}
+
+            // ---------------------- ACCESSORS ------------------------	
+
+            // ----------------------- MUTATORS ------------------------
+#ifdef LOGIC_COMPILER_FIX
+         public:
+            ErrorToken& operator=(const ErrorToken& r) const
+               { throw AlgorithmException(HERE, L"LogicLibrary compiler-fix code executed"); }
+#endif
+            // -------------------- REPRESENTATION ---------------------
+         public:
+            const GuiString Message,   // Error message
+                            Text;      // Erroneous token/line text
+            const UINT      Line;      // 1-base line number
+         };
+
+         /// <summary>Vector of error tokens</summary>
+         class LogicExport ErrorArray : public vector<ErrorToken> 
+         {
+            // --------------------- CONSTRUCTION ----------------------
+         public:
+            /// <summary>Create empty array</summary>
+            ErrorArray()
+            {}
+            /// <summary>Create array from single token</summary>
+            ErrorArray(const ErrorToken& e) {
+               push_back(e);
+            }
+
+            // ---------------------- ACCESSORS ------------------------	
+
+            // ----------------------- MUTATORS ------------------------
+         public:
+            /// <summary>Add an error to the array</summary>
+            ErrorArray& operator+=(const ErrorToken& t)
+            {
+               push_back(t);
+               return *this;
+            }
+
+            // -------------------- REPRESENTATION ---------------------
+
+         };
+      }
+   }
+}
+
+using namespace Logic::Scripts::Compiler;
