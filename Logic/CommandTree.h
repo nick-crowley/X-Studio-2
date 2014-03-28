c3950395a0af0094641ebd6f047e8f106d7c878a
Black Bear <Black Bear@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-03-28 23:11:43 +0000

    Finished expansion of 'dim' macros

--- Logic/CommandTree.h
+++ Logic/CommandTree.h
@@ -56,10 +56,13 @@ namespace Logic
             // --------------------- CONSTRUCTION ----------------------
          public:
             CommandTree();
-            CommandTree(CommandTree* parent, const CommandTree* target);
             CommandTree(Conditional cnd, CommandSyntaxRef syntax, ParameterArray& params, const CommandLexer& lex, UINT line, bool commented);
             CommandTree(Conditional cnd, CommandSyntaxRef syntax, ParameterArray& infix, ParameterArray& postfix, const CommandLexer& lex, UINT line, bool commented);
             ~CommandTree();
+
+         protected:
+            CommandTree(const CommandTree& parent, const CommandTree* target);
+            CommandTree(const CommandTree& m, Conditional cnd, CommandSyntaxRef syntax, ParameterArray& params);
 
             // ------------------------ STATIC -------------------------
          protected:
@@ -118,13 +121,13 @@ namespace Logic
             void           Verify(ScriptFile& script, ErrorArray& errors);
             
          protected:
+            CommandNodeList  ExpandDimArray(ScriptFile& script);
             void  ExpandMacros(ScriptFile& script, ErrorArray& errors);
             void  FinalizeLinkage(ErrorArray& errors);
             void  GenerateCommands(ScriptFile& script, ErrorArray& errors);
             void  IdentifyConstants(ScriptFile& script, ErrorArray& errors);
             void  IdentifyVariables(ScriptFile& script, ErrorArray& errors);
             void  IndexCommands(UINT& next);
-            void  InsertAfter(CommandTree* pos, CommandTree* cmd);
             void  InsertJump(NodeIterator pos, const CommandTree* target);
             void  LinkCommands(ErrorArray& errors);
             void  RevertCommandComment(CommandTree* child);
