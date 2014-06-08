3763cb80c713205a1cc3b132a852270176eedac9
xstudio.development@gmail.com <xstudio.development@gmail.com@0466a0bf-d0c3-799f-e171-cf66d25abde8>
2014-06-08 19:57:10 +0000

    Moved Event, ConsoleWnd, MapIterator, MatchData from 'Logic' to 'Logic::Utils' namespace for clarity.

--- trunk/Logic/MapIterator.hpp
+++ trunk/Logic/MapIterator.hpp
@@ -3,48 +3,51 @@
 
 namespace Logic
 {
-   /// <summary>Forward only iterator for accessing the values in a map collection</summary>
-   template <typename OBJ, typename COLL, typename ITER>
-   class MapIterator : public std::iterator<std::forward_iterator_tag, OBJ>
+   namespace Utils
    {
-      // ------------------------ TYPES --------------------------
+      /// <summary>Forward only iterator for accessing the values in a map collection</summary>
+      template <typename OBJ, typename COLL, typename ITER>
+      class MapIterator : public std::iterator<std::forward_iterator_tag, OBJ>
+      {
+         // ------------------------ TYPES --------------------------
    
-      // --------------------- CONSTRUCTION ----------------------
-   public:
-      /// <summary>Create new iterator</summary>
-      /// <param name="c">The collection</param>
-      /// <param name="pos">The initial position</param>
-      MapIterator(const COLL& c, ITER pos) : Collection(&c), Position(pos)
-      {
-      }
+         // --------------------- CONSTRUCTION ----------------------
+      public:
+         /// <summary>Create new iterator</summary>
+         /// <param name="c">The collection</param>
+         /// <param name="pos">The initial position</param>
+         MapIterator(const COLL& c, ITER pos) : Collection(&c), Position(pos)
+         {
+         }
 
-      // --------------------- PROPERTIES ------------------------
+         // --------------------- PROPERTIES ------------------------
 			
-		// ---------------------- ACCESSORS ------------------------
-   public:
-      OBJ& operator*() const                      { return Position->second; }
-      OBJ* operator->() const                     { return &Position->second; }
-      bool operator==(const MapIterator& r) const { return Collection==r.Collection && Position==r.Position; }
-      bool operator!=(const MapIterator& r) const { return Collection!=r.Collection || Position!=r.Position; }
+		   // ---------------------- ACCESSORS ------------------------
+      public:
+         OBJ& operator*() const                      { return Position->second; }
+         OBJ* operator->() const                     { return &Position->second; }
+         bool operator==(const MapIterator& r) const { return Collection==r.Collection && Position==r.Position; }
+         bool operator!=(const MapIterator& r) const { return Collection!=r.Collection || Position!=r.Position; }
 
-      // ----------------------- MUTATORS ------------------------
-   public:
-      MapIterator& operator++() 
-      { 
-         ++Position;
-         return *this;
-      }
+         // ----------------------- MUTATORS ------------------------
+      public:
+         MapIterator& operator++() 
+         { 
+            ++Position;
+            return *this;
+         }
 
-      MapIterator operator++(int) 
-      {
-         MapIterator tmp(*this); 
-         operator++(); 
-         return tmp;
-      }
+         MapIterator operator++(int) 
+         {
+            MapIterator tmp(*this); 
+            operator++(); 
+            return tmp;
+         }
             
-      // -------------------- REPRESENTATION ---------------------
-   private:
-      const COLL* Collection;
-      ITER        Position;
-   };
+         // -------------------- REPRESENTATION ---------------------
+      private:
+         const COLL* Collection;
+         ITER        Position;
+      };
+   }
 }
